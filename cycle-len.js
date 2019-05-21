0679c2a4ed3b350783c6ee3d6f178f1b5ff2fdde
xtango <xtango@users.noreply.github.com>
2019-05-21 08:26:26 -0400

    Update cycle-len.js

--- cycle-len.js
+++ cycle-len.js
@@ -1,6 +1,8 @@
 /**
- * Finds the cycle length in arr. Follow each elem to the index it point to. 
- * Stop when a cycle is found and return length of the cycle (or -1 if no cycle found)
+ * Finds the cycle length in arr, an array of integers. 
+ * Approach: Follow each elem to the index it point to. 
+ * Stop when a cycle is found & return length of the cycle (or -1 if no cycle found)
+ *
  * e.g.
  *    cycleLen([1,0]) -> 2
  *    cycleLen([1, 2, 0]) -> 3
@@ -11,9 +13,13 @@ function cycleLen(arr) {
     let idx = 0;
     let seq = 1;
 
-    // Visit Sequence is in the form { node: vistitNumber} 
-    // e.g. { 0: 1, 4: 2, 10, 3 } to mean 0 was visited 1st, and 4 was visited 2nd, 10 was visited 3rd.
+    // visitSequence is an object holding { node: visitNumber, ... } 
+    // e.g. { 0: 1, 4: 2, 10: 3 } means
+    //      0 was visited 1st
+    //      4 was visited 2nd
+    //      10 was visited 3rd.
     let visitSequence = {}; 
+ 
     while (!cycleFound && idx < arr.length) {
         visitSequence[idx] = seq;
         console.log(seq, idx, visitSequence[idx]);
@@ -26,5 +32,5 @@ function cycleLen(arr) {
 }
 
 // Tests
-if (cycleLen([1, 0]) === 2) console.log('passed')
-if (cycleLen([1, 2, 0]) === 3) console.log('passed')
+if (cycleLen([1, 0]) === 2) console.log('passed') else console.logi('failed')
+if (cycleLen([1, 2, 0]) === 3) console.log('passed') else console.logi('failed')

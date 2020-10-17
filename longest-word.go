76a7cdec471f278f139316ddd8d36618198e29c3
xtango <xtango@users.noreply.github.com>
2020-10-17 08:14:25 -0400

    Update longest-word.go

--- longest-word.go
+++ longest-word.go
@@ -6,16 +6,21 @@ import (
 	"strings"
 )
 
+/**
+ * Returns a list of valid words. In the future, this can pull from a file or API.
+ */
 func getDict() []string {
 	return []string{"go", "to", "god", "dog"}
 }
+
+type InvertedDict map[string][]string
 
 /**
  * Creates a { key -> [dictionaryWord]} object, where key is the sorted letters entry.
  * E.g. makeInvertedDict(['dog', 'god'] ) returns
  *      { dgo: [ 'god', 'dog']}
  */
-func makeInvertedDict(dict []string) map[string][]string {
+func makeInvertedDict(dict []string) InvertedDict {
 	m := make(map[string][]string)
 	for _, word := range dict {
 		sorted := sortWord(word)
@@ -35,13 +40,21 @@ func sortWord(w string) string {
 	return strings.Join(s, "")
 }
 
-//func longestWord(dict []string, hand string) {
-//	m := makeInvertedDict(dict)
+/**
+ * Returns combinations of the word
+ */
+//func combos(word string) []string {
+//
 //}
+
+func longestWord(invertedDict, hand string) {
+
+}
 
 func main() {
 	dict := getDict()
+	inverted := makeInvertedDict(dict)
 	fmt.Printf("LONGEST DICTIONARY WORD\n")
-	fmt.Printf("   Dictionary: %s\n", strings.Join(dict, " "))
-	fmt.Printf("     Inverted: %s", makeInvertedDict(dict))
+	fmt.Printf("Dictionary: %s\n", strings.Join(dict, " "))
+	fmt.Printf("  Inverted: %s", inverted)
 }

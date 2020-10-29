ec49aed123a5ba9f21edd7af34fc1d46f6c13e5d
xtango <xtango@users.noreply.github.com>
2020-10-29 13:31:36 -0400

    Update sudoku.ts

--- sudoku.ts
+++ sudoku.ts
@@ -1,6 +1,14 @@
 /**
  * Sodoku Solver
- * An empty cell is represented by 0.
+ * 
+ * "In classic sudoku, the objective is to fill a 9×9 grid with
+ * digits so that each column, each row, and each of the nine 3×3 subgrids that
+ * compose the grid (also called "boxes", "blocks", or "regions") contain all 
+ * of the digits from 1 to 9. The puzzle setter provides a partially completed 
+ * grid, which for a well-posed puzzle has a single solution."
+ *                                                              - Wikipedia
+ *
+ * Approach: brute force with backtracking. 
 */
 type Grid = number[][];
 
@@ -12,16 +20,30 @@ const isNumOkInCol = (g: Grid, x: number, n: number) => colSlice(g, x).every(el 
 
 const isNumOkInRow = (g: Grid, y: number, n: number) => g[y].every(el => el !== n);
 
-const isNumOkInSector = (g: Grid, y: number, x: number, n: number) => true; // todo
+/**
+ * Each subgrid should have distinct (no dups) numbers, 1..9
+ */
+const isNumOkInSubGrid = (g: Grid, y: number, x: number, n: number): boolean => {
+    const xOffset = Math.floor(x / 3) * 3; 
+    const yOffset = Math.floor(y / 3) * 3;
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (g[i+yOffset][j+xOffset] === n) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
 const isNumValid = (g: Grid, y: number, x: number, n: number): boolean =>
     isNumOkInCol(g, x, n)
     && isNumOkInRow(g, y, n)
-    && isNumOkInSector(g, y, x, n)
+    && isNumOkInSubGrid(g, y, x, n)
 
 const parse = (st: string[]): number[][] => st.map(l => l.split(' ').map(x => Number(x)));
 
-const prettyYXN = (y: number, x: number , n: number): string => `(${y}, ${x}): ${n}`;
+const prettyYXN = (y: number, x: number, n: number): string => `(${y}, ${x}): ${n}`;
 /**
  * Returns the y, x tuple of the next empty cell or [-1, -1] when none.
  */
@@ -36,13 +58,16 @@ const nextEmpty = (grid: Grid): CoordinateYX => {
 }
 
 const solve = (g: Grid) => {
-    let backTrackCount = 0;
 
-    const solveRecur = (g: Grid): boolean => {
-        console.log(pretty(g));
-     
+    const solveRecur = (g: Grid, backtrackCount: number = 0, depth: number = 0): boolean => {
+        console.log(`[solveRecur] recurDepth: ${depth}, backtrack: ${backtrackCount} ${pretty(g)}`);
+        if (depth > 10) {
+            console.log('[solveRecur] Abort: max recur depth reached');
+            return true;
+        }
+
         const [y, x] = nextEmpty(g);
-        console.log(`[solveRecur] ${prettyYXN(y,x, 0)}`);
+        console.log(`[solveRecur] ${prettyYXN(y, x, 0)}`);
 
         if (y === -1) {
             return true; // No more empty cells
@@ -51,23 +76,23 @@ const solve = (g: Grid) => {
         // Brute force: try out nums 1..9 in the empty cell
         for (let n = 1; n <= 9; n++) {
             if (isNumValid(g, y, x, n)) {
-                console.log(`...${prettyYXN(y,x, n)} Valid`);
+                console.log(`...${prettyYXN(y, x, n)} -> valid`);
                 // Change value for the next grid state
                 g[y][x] = n;
 
                 // Recurse with the next grid state
-                if (solveRecur(g)) {
+                if (solveRecur(g, backtrackCount, depth + 1)) {
                     return true;
                 } else {
-                    // Ran into a conflict. Let's back track
+                    // A conflict. Let's back track
                     console.log(`...${prettyYXN(y, x, n)} -> conflict backtrack`);
                     g[y][x] = 0; // Change back to 0 (was changed to n, see above)
-                    backTrackCount++;
+                    backtrackCount++;
                 }
             } else {
-                console.log(`...${prettyYXN(y,x, n)} -> invalid, try another...`);
+                //console.log(`...${prettyYXN(y, x, n)} -> invalid, try another...`);
             }
-             
+
         }
         return false;
     }
@@ -94,6 +119,7 @@ const pretty = (g: Grid): string => {
 
 /**
  * Tests
+ * An empty cell is represented by 0.
  */
 const SIMPLE = parse([
     '0 4 0 0 0 0 1 7 9',
@@ -111,4 +137,6 @@ const SIMPLE = parse([
 console.log(SIMPLE[0][1] === 4 ? 'passed' : 'failed');
 console.log(colSlice(SIMPLE, 3)[6] === 4 ? 'passed' : 'failed');
 console.log(nextEmpty(SIMPLE).join('_') === '0_0' ? 'passed' : 'failed');
+console.log(isNumOkInSubGrid(SIMPLE, 3, 0, 8) === false ? 'passed' : 'failed');
+console.log(isNumOkInSubGrid(SIMPLE, 3, 0, 7) === true ? 'passed' : 'failed');
 solve(SIMPLE);

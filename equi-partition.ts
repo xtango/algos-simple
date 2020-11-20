41bf2a386ad869ad9aabe214ca901d48d58a5949
xtango <xtango@users.noreply.github.com>
2020-11-19 23:59:27 -0500

    Update equi-partition.ts

--- equi-partition.ts
+++ equi-partition.ts
@@ -1,5 +1,3 @@
-/*************** WIP ******************/
-
 /**
  * Determine whether a given set can be partitioned into two subsets such that the sum of elements in both subsets is the same. 
  */
@@ -8,17 +6,9 @@ const sum = (arr: number[]): number => {
 }
 
 /**
- * @example canPart([1, 5, 11, 5]) -> true because sum(1, 5, 5) = sum (11)
+ * @example equiPartitionable([1, 5, 11, 5]) -> true because sum(1, 5, 5) = sum (11)
  */
-const canPartition = (arr: number[]): boolean => {
-    const sorted = arr.sort();  // E.g. 1, 5, 5, 11
-    
-    // Abort immediately if sum is odd, since to be equi-partionable, sum should be even.
-    const fullSum = sum(arr);
-    if (fullSum % 2 > 0) {
-        return false; 
-    }
-
+const equiPartitionable = (arr: number[]): boolean => {
     /**
      * We move from numbers from the left hand side to the right.
      * Left           Right     Left Sum    RightSum
@@ -32,36 +22,48 @@ const canPartition = (arr: number[]): boolean => {
      * [3, 4, 6]
      * [3, 4]         [6]       7           6      --> return false when leftSum > rightSum 
      * 
-     * param i: right index. We start from array lenght -1 and decrement it.
+     * @param i Right index. We start from array length -1 and decrement it.
      */
     const partitionHelper = (i: number, leftSum: number): boolean => {
         const rightSum = fullSum - leftSum;
 
-        // Abort immediate: Too large case
-        if (leftSum < sorted[i]) { // e.g. 1, 2, 5 where  1 + 2 < 5
+        console.log('[partition] i, leftSum, rightSum', i, leftSum, rightSum);
+
+        if (i === 0) {
             return false;
-        } 
-        
-        if (leftSum > rightSum) { // e.g  sum(1, 2, 4) > sum(5)
-            const leftIdx = arr.length() - i;
-            return partitionHelper(i - 1, leftSum - sorter[leftIdx] ) // move leftmost to the right
         }
 
-        if (leftSum === rightSum) {
+        // Abort immediate: Too large case
+        if (leftSum < arr[i]) { // e.g. 1, 2, 5 where  1 + 2 < 5
+            return false;
+        }
+
+        if (leftSum > rightSum) { // e.g  sum(1, 2, 4) > sum(5)
+            const leftIdx = arr.length - i;
+            return partitionHelper(i - 1, leftSum - arr[leftIdx]) // move leftmost to the right
+        } else if (leftSum === rightSum) {
             return true;
         }
 
         return false;
     }
 
-    return partitionHelper(arr.length -1, fullSum);
+    console.log('EQUI PARTITIONABLE', arr);
+    arr.sort((a, b) => a - b);
+
+    // Abort immediately if sum is odd, since to be equi-partionable, sum should be even.
+    const fullSum = sum(arr);
+    if (fullSum % 2 > 0) {
+        return false;
+    }
+    return partitionHelper(arr.length - 1, fullSum - arr[arr.length - 1]);
 }
 
 
 /**
  * Tests
  */
-console.log(sum([1,2,3]) === 6 ? 'Passed' : 'Failed')
-
-
-
+console.log(sum([1, 2, 3]) === 6 ? 'Passed' : 'Failed');
+console.log(equiPartitionable([1, 5, 11, 5]) === true ? 'Passed' : 'Failed')
+console.log(equiPartitionable([1, 1, 3, 4, 7]) === true ? 'Passed' : 'Failed')
+console.log(equiPartitionable([2, 3, 4, 6]) === false ? 'Passed' : 'Failed');

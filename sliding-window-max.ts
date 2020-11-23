dd8a81f51ebeef535e146ea6ded2a35bb598691d
xtango <xtango@users.noreply.github.com>
2020-11-22 21:33:08 -0500

    Update sliding-window-max.ts

--- sliding-window-max.ts
+++ sliding-window-max.ts
@@ -14,13 +14,23 @@
  */
 type DEQ = number[];
 
+const deqBack = (q: DEQ) => q[q.length - 1];
+
+const deqFront = (q: DEQ) => q[0];
+
+/**
+ * Returns a new q without the right-most elem.
+ */
+const deqPopRight = (q: DEQ): DEQ => q.slice(0, q.length - 1);
+
+
 /**
  * Pops at right until we find a larger elem. Then pushes at right.
  * 
  * @param nums The sequence of values.
- * @param q Queue of indices to nums.  When de-referenced, the resulant
- *          values are in descending order.
- * @param index The current index into 'nums'.
+ * @param qIndices Queue of indices to nums.  When de-referenced, the resulant
+ *                 values are in descending order.
+ * @param rightIdx The current index into 'nums'.
  * 
  * Though 'q' stores indices to values in 'nums', for easier understanding
  * these examples use values instead of indices:
@@ -29,26 +39,61 @@ type DEQ = number[];
  */
 const deqPushSorted = (
     nums: Readonly<number[]>,
-    q: DEQ,
-    index: number): DEQ => {
-    while (q.length > 0
-        // When current num is greater than the back of queue, pop
-        // (the smaller num is no longer relevant to determine max)
-        && nums[index] > nums[q[q.length - 1]]) {
-        q = deqPopRight(q);
+    qIndices: DEQ,
+    rightIdx: number,
+    k: number): DEQ => {
+    while (qIndices.length > 0
+        && (
+            // Pop when 
+            // a. Current num is greater than the back of queue
+            // (the smaller num is no longer relevant to determine max)
+            nums[rightIdx] > nums[deqBack(qIndices)]
+            ||
+            // or b. When q's back in no longer in the window
+            (deqBack(qIndices) < rightIdx - k)
+        )) {
+        qIndices = deqPopRight(qIndices);
+        console.log('...[pop] qIndices, vals...', qIndices, qIndices.map(x => nums[x]))
     }
-    q.push(index);
-    return q;
+
+    qIndices.push(rightIdx);
+    console.log('...[push] qIndices, vals..', qIndices, qIndices.map(x => nums[x]))
+    return qIndices;
 }
+
+const prettyWindow = (nums: Readonly<number[]>, k: Readonly<number>, rightIdx: number): string => {
+    const prettyNum = (i: number) => {
+        const pre = i === rightIdx - k + 1 ? '[' : '';
+        const post = i === rightIdx ? ']' : '';
+        return `${pre}${nums[i].toString()}${post} `
+    };
+    return nums.map((undefined, i) => prettyNum(i)).join('');
+}
+
 /**
- * Returns a new q without the right-most elem.
+ * 
  */
-const deqPopRight = (q: DEQ): DEQ => q.slice(0, q.length - 1);
+const slideMax = (nums: Readonly<number[]>, k: Readonly<number>): number[] => {
+    console.log('MAX OF SLIDING WIN nums, k', nums, k)
+    let q: DEQ = []
+    const maxList: number[] = [];
+    for (let i = 0; i < nums.length; i++) {
+        console.log(`--------i: ${i}, window--------- ${prettyWindow(nums, k, i)}`);
+        q = deqPushSorted(nums, q, i, k);
+
+        if (i >= k) {
+            maxList.push(nums[deqFront(q)])
+        }
+    }
+
+    return maxList;
+}
 
 /**
  * Assertions
  */
-console.log(deqPopRight([1, 2, 3]).join(',') == '1,2');
-console.log(deqPushSorted([3, 2, 0], [], 0).join(',') === '0');
-console.log(deqPushSorted([3, 2, 0, 1, 4], [0, 1, 2], 3).join(',') === '0,1,3');
-console.log(deqPushSorted([3, 2, 0, 1, 4], [0, 1, 2], 4).join(',') === '4');
+// console.log(deqPopRight([1, 2, 3]).join(',') == '1,2');
+// console.log(deqPushSorted([3, 2, 0], [], 0).join(',') === '0');
+// console.log(deqPushSorted([3, 2, 0, 1, 4], [0, 1, 2], 3).join(',') === '0,1,3');
+// console.log(deqPushSorted([3, 2, 0, 1, 4], [0, 1, 2], 4).join(',') === '4');
+console.log(slideMax([1, 4, 0, 3, 2], 3));

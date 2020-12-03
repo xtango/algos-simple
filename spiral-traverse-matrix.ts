aa740e36546c5e3f03f06fe65432bbf538b01e8b
xtango <xtango@users.noreply.github.com>
2020-12-02 23:31:45 -0500

    Update spiral-traverse-matrix.ts

--- spiral-traverse-matrix.ts
+++ spiral-traverse-matrix.ts
@@ -1,21 +1,23 @@
 /**
- * SPIRAL
- * Traverse a matrix in spiral clock-wise order.
+ * SPIRAL WALK
+ * Traverse a matrix in a clock-wise spiral order.
  * 
+ * Example
  * 1 2 3 
  * 4 5 6    -> 1 2 3 6 9 9 7 4 5
  * 7 8 9  
  */
+
 type DirOffset = { y: number, x: number };
 
 interface Bounds { yMin: number, yMax: number, xMin: number, xMax: number };
 
 /**
  * Return the new offsets on a change direction. Clockwise walk, so turn right
- *                   R      ->  D       ->  L        ->  U      -> R ...
- * Offsets y, x     0, 1       1, 0        0, -1       -1, 0
+ *                  R     ->  D       ->  L       ->  U       -> R ...
+ * Offsets y, x     0, 1      1, 0        0, -1      -1, 0
  */
-const nextDirectionOffset = (offset: DirOffset): DirOffset => { return { y: offset.x, x: -offset.y } }
+const nextDirection = (offset: DirOffset): DirOffset => { return { y: offset.x, x: -offset.y } }
 
 const shrinkBounds = (bounds: Bounds, dir: DirOffset): Bounds => {
     return {
@@ -29,36 +31,43 @@ const shrinkBounds = (bounds: Bounds, dir: DirOffset): Bounds => {
 const outOfBounds = (y: number, x: number, bounds: Bounds) =>
     y > bounds.yMax || y < bounds.yMin || x > bounds.xMax || x < bounds.xMin;
 
-const spiralWalk = (mat: number[][]) => {
 
+const spiralWalk = (mat: number[][]): number[] => {
     // Inclusive indices
-    const bounds: Bounds = {
+    let bounds: Bounds = {
         yMin: 0,
         yMax: mat.length - 1,
         xMin: 0,
         xMax: mat[0].length - 1
     }
 
-    let offset: DirOffset = { y: 0, x: 1 } // Move right initially
+    let direction: DirOffset = { y: 0, x: 1 } // Move right initially
 
     let y = 0;
     let x = 0;
     let i = 0;
-    while (i < mat.length * mat[0].length) {
+    const out = [];
+    const MAX_I = mat.length * mat[0].length;
+    
+    while (i < MAX_I) {
+        out.push(mat[y][x]);
         console.log(`(${y}, ${x}) -> ${mat[y][x]}`);
-        // Out of bounds, change direction and shrink bounds
-        if (outOfBounds(y + offset.y, x + offset.x, bounds)) {
-            offset = nextDirectionOffset(offset);
-
+        // Out of bounds, shrink and change direction
+        if (outOfBounds(y + direction.y, x + direction.x, bounds)) {
+            bounds = shrinkBounds(bounds, direction);
+            direction = nextDirection(direction);
         }
-        y = y + offset.y;
-        x = x + offset.x;
+        y = y + direction.y;
+        x = x + direction.x;
         i++;
     }
-
+    return out;
 }
 
-spiralWalk(
+/**
+ * TESTS
+ */
+console.log('Test', spiralWalk(
     [[1, 2, 3],
     [4, 5, 6],
-    [7, 8, 9]]);
+    [7, 8, 9]]).join(' ') == '1 2 3 6 9 8 7 4 5');

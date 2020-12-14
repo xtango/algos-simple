1432652187b7d48c88347421d9ba8c6662973343
xtango <xtango@users.noreply.github.com>
2020-12-13 21:38:26 -0500

    Update 087 - rules-valid.ts

--- daily-coding/087 - rules-valid.ts
+++ daily-coding/087 - rules-valid.ts
@@ -1,5 +1,5 @@
 /**
- * #87 - RULES VALID? This problem was asked by Uber.
+ * #87 RULES VALID? This problem was asked by Uber.
  * 
  * A rule looks like this:
  *      'A NE B' This means this means point A is located northeast of point B.
@@ -16,33 +16,42 @@
  * METHODOLOGY
  * We use a graph with edges weights representing directions.
  * On adding a rule, we check if edge weights contradict existing weights.
- *
  */
-interface DestinationDirection { [key: string]: { y: number; x: number } }
-
-interface GraphNode {
-    edges: DestinationDirection;
-}
-
+interface DestDirection { [key: string]: { y: number; x: number } }
+interface GraphNode { edges: DestDirection; }
 interface Graph { [key: string]: GraphNode }
 
-const DIRECTION: DestinationDirection = {
+/**
+ *      ---> X
+ *      |
+ *      v Y
+ */
+const DEST_OFFSET: DestDirection = {
     'N': { y: -1, x: 0 },
-    'S': { y: 1, x: 0 }
+    'NE': { y: -1, x: 1 },
+    'NW': { y: -1, x: -1 },
+    'E': { y: 0, x: 1 },
+    'S': { y: 1, x: 0 },
+    'SE': { y: 1, x: 1 },
+    'SW': { y: -1, x: -1 },
+    'W': { y: 0, x: -1 },
 }
 
 /**
- * Parse rules that are in the form <from> <direction> <to>.
- * Create nodes if they don't exist.
+ * Parses rule in the form <from> <direction> <to>.
+ */
+const tokenize = (rule: string) => {
+    const tokens = rule.split(' ');
+    return [tokens[0], tokens[1], tokens[2]];
+}
+
+/**
+ * Create nodes and edges and compares existing edge weights.
  * @example: isValid(['A S B', 'B S A']) -> false
  *           Rule 1: 'A S B', A is south of B, gives edges1 = {
  *		           A: { B: {y: 1, x: undefined} }   // A is south of B
  *		           B: { A: {y: -1, x: undefined}}}  // B is north of A
- *
- *                     B
- *             south |   ^
- *   	             v   | north
- *                     A
+
  *          Rule 2: 'B S A' gives edges2 = {A: {y: 0, x: 0}, B: {y: 1}}
  *          which contradicts rule 1: graph1.B.y != graph2..B.y
  * 
@@ -51,41 +60,77 @@ const DIRECTION: DestinationDirection = {
 const isValid = (rules: string[]): boolean => {
     const graph: Graph = {};
 
-    const createNodesWhenNotExists = (start: string, dest: string) => {
-        if (!graph[start]) {
-            graph[start] = { edges: {} }
-        }
-        if (graph[dest] === undefined) {
-            graph[dest] = { edges: {} }
-        }
+    const createNode = (key: string) => {
+        graph[key] = { edges: {} }
     }
 
     /**
      * Set edge soure to target and vice versa.
+     *                     B
+     *             south |   ^
+     *   	             v   | north
+     *                     A
      */
-    const addEdges = (start: string, dest: string, dir: string): void => {
-        graph[start].edges[dest] = DIRECTION[dir];
-        graph[dest].edges[start] = { y: DIRECTION[dir].y * -1, x: DIRECTION[dir].x * -1};
+    const addEdge = (start: string, dest: string, y: number, x: number): void => {
+        graph[start].edges[dest] = { y, x };
+    }
+
+    /**
+     * Todo: Must improve by checking that every node from start to destination
+     */
+    const sameDirection = (start: string, dest: string, dir: string): boolean => {
+        const edge = graph[start].edges[dest];
+        if (edge === undefined) {
+            return true
+        } else {
+            return (edge.y === 0 || DEST_OFFSET[dir].y === 0 || edge.y === DEST_OFFSET[dir].y)
+                &&
+                (edge.x === 0 || DEST_OFFSET[dir].x === 0 || edge.x === DEST_OFFSET[dir].x);
+        }
     }
 
     /**
      * Adds a rule
      */
     const add = (rule: string) => {
-        const tokens = rule.split(' ');
-        const [start, dir, target] = [tokens[0], tokens[1], tokens[2]];
-        createNodesWhenNotExists(start, target);
-        addEdges(start, target, dir);
+        console.log(`[add] ${rule}`);
+        const [start, dir, dest] = tokenize(rule);
 
-        console.log('graph: ', graph);
-        return true;
+        [start, dest].forEach(node => {
+            if (!graph[node]) {
+                createNode(node);
+            }
+        });
+
+        let valid = true;
+        if (sameDirection(start, dest, dir)) {
+            addEdge(start, dest, DEST_OFFSET[dir].y, DEST_OFFSET[dir].x);
+            addEdge(dest, start, DEST_OFFSET[dir].y * -1, DEST_OFFSET[dir].x * -1);
+        } else {
+            valid = false;
+        }
+        //console.log('[add rule] valid, graph', valid, graph);
+        return valid;
     }
 
+    console.log(`\nVALIDATE RULE [ ${rules.map(x=>`'${x}'`).join(', ')} ]`);
     return rules.every(r => add(r))
 }
 
 /**
  * Tests
  */
-console.log(isValid(['A S B']);
-//console.log(isValid(['A S B', 'B S A']));
+console.log(isValid(['A S B']) === true ? 'passed' : 'failed');
+
+console.log(isValid(['A S B', 'B S A']) === false ? 'passed' : 'failed');
+
+/**
+ *   C
+ *    \
+ *      A
+ *     /
+ *   B
+ */
+console.log(isValid(['A NE B', 'A SW C']) === true ? 'passed' : 'failed');
+
+console.log(isValid(['A NW B', 'A N B']) === true ? 'passed' : 'failed');

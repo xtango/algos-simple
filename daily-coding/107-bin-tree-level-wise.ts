85e36f9f96afda341e5c89579a26f53738d9431e
xtango <xtango@users.noreply.github.com>
2021-01-02 11:50:20 -0500

    Update 107-bin-tree-level-wise.ts

--- daily-coding/107-bin-tree-level-wise.ts
+++ daily-coding/107-bin-tree-level-wise.ts
@@ -19,10 +19,37 @@ interface BinTreeNode {
     right?: BinTreeNode;
 }
 
+/**
+ * Returns node values level-wise, i.e. in sorted order
+ */
 const levelOrder = (root: BinTreeNode): number[] => {
+    
+    
+    const helper = (node: BinTreeNode) => {
+        console.log('[helper]->', root.val);
+        const q: BinTreeNode[] = [];
+        if (node.left) {
+            q.push(node.left);
+        }
+        console.log('[helper] q: ', q);
+        if (node.right) {
+            q.push(node.right);
+        }
+        console.log('[helper] q: ', q);
+        while (q.length > 0) {
+            const head = q.shift();
+            helper(head as BinTreeNode);
+        }
+    }
+
+    helper(root);
     return [];
+
 }
 
+/**
+ * ASSERTIONS
+ */
 levelOrder({
     val: 1,
     left: { val: 2 },

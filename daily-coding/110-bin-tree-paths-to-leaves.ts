a43c3929a52fed19e4e73c010f3e9b93f747176c
xtango <xtango@users.noreply.github.com>
2021-01-06 21:49:24 -0500

    Update 110-bin-tree-paths-to-leaves.ts

--- daily-coding/110-bin-tree-paths-to-leaves.ts
+++ daily-coding/110-bin-tree-paths-to-leaves.ts
@@ -1,20 +1,18 @@
 /**
  *              #110 [Medium] BIN TREE PATHS FROM ROOT TO LEAVES
  * 
- *               !!!!!!!!! WORK IN PROGRESS !!!!!!
+ * This problem was asked by Facebook.
  * 
-* This problem was asked by Facebook.
-* 
-* Given a binary tree, return all paths from the root to leaves.
-* For example, given the tree:
-*    1
-*   / \
-*  /   \
-* 2     3
-*      / \
-*     4   5
-* Return [[1, 2], [1, 3, 4], [1, 3, 5]].
-*/
+ * Given a binary tree, return all paths from the root to leaves.
+ * For example, given the tree:
+ *      1
+ *     / \
+ *    /   \
+ *   2     3
+ *        / \
+ *       4   5
+ * Return [[1, 2], [1, 3, 4], [1, 3, 5]].
+ */
 
 interface BinTreeNode {
     val: number;
@@ -22,31 +20,66 @@ interface BinTreeNode {
     right?: BinTreeNode;
 }
 
-const leafPaths = (root: BinTreeNode) => {
-    const traverse = (node: BinTreeNode, leftPath: number[] = [], rightPath: number[] = []) => {
-        console.log('traverse', node.val);
+type Paths = number[][];
 
-        if (!node.left && !node.right) { // leaf
-            console.log('-> leaf, leftPath, rightPath', leftPath, rightPath);
+const rootToLeavesPaths = (root: BinTreeNode): Paths => {
+    const allPaths: Paths = [];
+
+    /**
+     * Depth first recursive traversal
+     */
+    const traverse = (node: BinTreeNode, path: number[] = [], depth: number = 0) => {
+        console.log(`[depth ${depth}] Node ${node.val}`);
+
+        if (!node.left && !node.right) { // Reached leaf
+            console.log('----Leaf,path-->', node.val, path);
+            allPaths.push(path);
         } else {
             if (node.left) {
-                traverse(node.left, leftPath.concat(node.left.val), []);
+                traverse(node.left, path.concat(node.left.val), depth + 1);
             }
             if (node.right) {
-                traverse(node.right, [], rightPath.concat(node.right.val));
+                traverse(node.right, path.concat(node.right.val), depth + 1);
             }
         }
     }
 
-    traverse(root, [root.val], [root.val])
+    console.log(':'.repeat(30));
+    traverse(root, [root.val]);
+    return allPaths;
 }
+
 /**
  * ASSERTIONS
  */
-const binTree1: BinTreeNode = {
-    val: 1,
-    left: { val: 2 },
-    right: { val: 3, left: { val: 4 }, right: { val: 5 } }
-};
+console.log(JSON.stringify(rootToLeavesPaths(
+    {
+        val: 1,
+        left: { val: 2 },
+        right: { val: 3 }
+    })) === '[[1,2],[1,3]]');
 
-leafPaths(binTree1);
+console.log(JSON.stringify(rootToLeavesPaths(
+    {
+        val: 1,
+        left: { val: 2 },
+        right: {
+            val: 3,
+            left: { val: 4 },
+            right: { val: 5 }
+        }
+    })) === '[[1,2],[1,3,4],[1,3,5]]');
+
+console.log(JSON.stringify(rootToLeavesPaths(
+    {
+        val: 1,
+        left: { val: 2 },
+        right: {
+            val: 3,
+            left: {
+                val: 4,
+                right: { val: 6 }
+            },
+            right: { val: 5 }
+        }
+    })) === '[[1,2],[1,3,4,6],[1,3,5]]');

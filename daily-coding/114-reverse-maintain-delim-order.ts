009fb3c664a9e6a8dc7466b508cfcca80a2f2e9a
xtango <xtango@users.noreply.github.com>
2021-01-10 12:48:12 -0500

    Update 114-reverse-maintain-delim-order.ts

--- daily-coding/114-reverse-maintain-delim-order.ts
+++ daily-coding/114-reverse-maintain-delim-order.ts
@@ -10,14 +10,20 @@
  * Follow-up: Does your solution work for the following cases:
  * "hello/world:here/",
  * "hello//world:here"
+ * 
+ * CONCEPTION MODEL
+ * Use 2 stacks, one for words and one for delimiters.
  */
-
 
 type State = { stack: string[], accum: string };
 
+const newState = () => { return { stack: [], accum: '' } };
+
+/**
+ * Reverses input while mainting the relative order of delims in input.
+ */
 const reverseWithDelimeters = (input: string, delims: string): string => {
-    const word: State = { stack: [], accum: '' };
-    const delim: State = { stack: [], accum: '' };
+    const [word, delim]: [State, State] = [newState(), newState()];
 
     console.log(`${'='.repeat(15)} REVERSE ${'='.repeat(15)}\n${input}`);
 
@@ -29,6 +35,12 @@ const reverseWithDelimeters = (input: string, delims: string): string => {
                 word.stack.push(word.accum);
                 word.accum = '';
             }
+
+            // Special case for last char
+            if (i === input.length - 1) {
+                delim.stack.push(delim.accum + input[i]);
+            }
+
             delim.accum += input[i];
         } else { // Not delim
             if (word.accum.length === 0 && delim.accum.length > 0) {
@@ -47,19 +59,32 @@ const reverseWithDelimeters = (input: string, delims: string): string => {
         console.log(`[${i} ${input[i]}]\nWord ${JSON.stringify(word)} \nDelim ${JSON.stringify(delim)}`);
     }
 
+    return formatReversed(word, delim);
+}
 
-    let reversed = word.stack[word.stack.length - 1];
+/**
+ * Returns the reverese string from the word and delim states.
+ */
+const formatReversed = (word: State, delim: State): string => {
+    let reversed: string = word.stack[word.stack.length - 1];
     let delimIdx = 0;
     for (let i = word.stack.length - 2; i >= 0; i--) {
-        reversed += delim.stack[delimIdx] + word.stack[i];
+        reversed += delim.stack[delimIdx] + word.stack[i]; // (i <= word.stack.length ? word.stack[i] : '');
         delimIdx++;
+    }
+
+    // Special case when the last token is delim, e.g. '//'
+    if (delimIdx < delim.stack.length) {
+        reversed += delim.stack[delimIdx];
     }
     return reversed;
 }
 
+
 /**
  * ASSERTIONS
  */
-// console.log(reverseWithDelimeters('hello/world:here', ':/') === 'here/world:hello'); // passes
-// console.log(reverseWithDelimeters("hello//world:here", ':/') === 'here//world:hello'); // passes
-console.log(reverseWithDelimeters('hello/world:here/', ':/')); // fails
+console.log(reverseWithDelimeters('hello/world:here', ':/') === 'here/world:hello');
+console.log(reverseWithDelimeters("hello//world:here", ':/') === 'here//world:hello');
+console.log(reverseWithDelimeters('hello/world:here/', ':/') === 'here/world:hello/');
+console.log(reverseWithDelimeters('hello/world:here//', ':/') === 'here/world:hello//');

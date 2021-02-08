f817f437d49c8338bfc73fb8fbb9f2b7d348554b
xtango <xtango@users.noreply.github.com>
2021-02-08 18:13:02 -0500

    Create 145-linked-list-swap-every-2.ts

---
+++ daily-coding/145-linked-list-swap-every-2.ts
@@ -0,0 +1,6 @@
+/**
+ *          #145 [Easy] - LINKED LIST SWAP
+ *
+ * Given the head of a singly linked list, swap every two nodes and return its head.
+ *For example, given 1 -> 2 -> 3 -> 4, return 2 -> 1 -> 4 -> 3.
+ */

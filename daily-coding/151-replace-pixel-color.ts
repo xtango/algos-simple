168fcc18f55effec0a32fb7694f6a8705ce304e7
xtango <xtango@users.noreply.github.com>
2021-02-16 01:00:27 -0500

    Update 151-replace-pixel-color.ts

--- daily-coding/151-replace-pixel-color.ts
+++ daily-coding/151-replace-pixel-color.ts
@@ -10,6 +10,7 @@
  * W W W
  * W W W
  * B B B
+ * 
  * Becomes
  * 
  * B B G
@@ -19,11 +20,7 @@
  */
 
 /**
- * (y, x)
- * 
- * --> x
- * |
- * v y
+ * In the form (y, x)
  */
 type ImageMatrix = string[][];
 
@@ -33,76 +30,99 @@ const getOffsets = (): number[][] => {
     const deltas = [];
     for (let r = -1; r <= 1; r++) {
         for (let c = -1; c <= 1; c++) {
-            if (!(r === 0 && c === 0)) {
-                deltas.push([r, c]);
-            }
+            deltas.push([r, c]);
         }
     }
     return deltas;
 }
 
 /**
- * Returns a matrix filled with undefined.
+ * Returns a matrix filled with false
  */
-const newMatrix = (len: number ) => [...Array(len)].map(e => Array(len));
+const newVisited = (yLen: number, xLen: number) => [...Array(yLen)]
+    .map(e => Array(xLen).fill(false));
 
 /**
- * 
+ * True when within bounds
  */
-const replace = (image: ImageMatrix, pixelRowCol: RowCol, toColor: string): ImageMatrix => {
+const withinBounds = (image: ImageMatrix, y: number, x: number): boolean =>
+    y > -1 &&
+    y < image.length &&
+    x > -1 &&
+    x < image[0].length;
+
+/**
+ * Has every pixel been vistied?
+ */
+const visitedAll = (visited: boolean[][]): boolean => {
+    const rows = visited.map(y => y.every(x => x === true));
+    return rows.every(elem => elem === true);
+}
+
+/**
+ * Replaces the color of the pixel specified in pixelYX and all adjacent same colored pixels.
+ */
+const replaceColor = (image: ImageMatrix, pixelYX: RowCol, toColor: string): ImageMatrix => {
     const offsets = getOffsets();
-    const fromColor = image[pixelRowCol[0]][pixelRowCol[1]];
-    const MAX_DEPTH = 2;
-    const visited = newMatrix(image.length);
+    const fromColor = image[pixelYX[0]][pixelYX[1]];
+    const MAX_DEPTH = 15; // Abort when greater
 
-    console.log('vistied', visited);
-
-    const withinBounds = (y: number, x: number): boolean =>
-        y > -1 &&
-        y < image.length &&
-        x > -1 &&
-        x < image.length;
+    // Initialize to false
+    const visited = newVisited(image.length, image[0].length);
 
     const pretty = (row: number, col: number, color: string): string =>
-        `(${row},${col}): ${color}${color === fromColor ? `->${toColor}` : ''}`;
+        `(${row},${col}): ${color}${color === fromColor ? ` replaced with ${toColor}` : ''}`;
 
     /**
-     * Helper func to be called recurrsively to do an in-place replace of 
-     * adjacent pixels that have the same fromColor
+     * Helper func that is called recurrsively. Mutates the image matrix in-place, replacing
+     * adjacent pixels whose color is fromColor.
      */
     const replaceHelper = (y: number, x: number, depth: number) => {
-        console.log(`[depth: ${depth}] replaceHelper(${y}, ${x})`);
-        visited[y][x] = true;
+        // console.log(`[depth: ${depth}] replaceHelper(${y}, ${x})`);
         if (depth > MAX_DEPTH) {
             return; // abort
         }
-        offsets.forEach(offset => {
-            const color = image[y][x];
-            if (color === fromColor && color !== toColor) {
-                image[y][x] = toColor;
-            }
 
-            const adjY = y + offset[0];
-            const adjX = x + offset[1];
-            if (withinBounds(adjY, adjX) && !visited[y][x]) {
-                console.log(`\t\tadjacent=${pretty(adjY, adjX, color)}`);
-                replaceHelper(adjY, adjX, depth + 1);
+        if (visitedAll(visited)) {
+            // console.log('Visited all pixels', visited);
+            return;
+        }
+
+        visited[y][x] = true;
+        //console.log(`\tvisit${pretty(y, x, image[y][x])}`);
+        if (image[y][x] === fromColor) {
+            image[y][x] = toColor;
+        }
+
+        // Recurse through all adjacent pixels that haven't been visited
+        offsets.forEach(offset => {
+            const [adjacentY, adjacentX] = [y + offset[0], x + offset[1]];
+            if (withinBounds(image, adjacentY, adjacentX) && !visited[adjacentY][adjacentX]) {
+                replaceHelper(adjacentY, adjacentX, depth + 1);
             }
         })
     }
 
-    replaceHelper(pixelRowCol[0], pixelRowCol[1], 0);
+    replaceHelper(pixelYX[0], pixelYX[1], 0);
     return image;
 }
-
 
 /**
  * ASSERTIONS
  */
-replace([
+console.log(visitedAll(newVisited(4, 6)) === false);
+console.log(visitedAll([[false, true], [true, false]]) === false);
+console.log(visitedAll([[true, true], [true, true]]) === true);
+
+const TEST_IMAGE = [
     ['B', 'B', 'W'],
     ['W', 'W', 'W'],
     ['W', 'W', 'W'],
-    ['B', 'B', 'B']],
-    [2, 2],
-    'G');
+    ['B', 'B', 'B']];
+
+console.log(withinBounds(TEST_IMAGE, 3, 3) === false);
+console.log(
+    JSON.stringify(
+        replaceColor(TEST_IMAGE, [2, 2], 'G')
+    ) === `[["B","B","G"],["G","G","G"],["G","G","G"],["B","B","B"]]`
+);

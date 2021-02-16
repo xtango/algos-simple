9dda291c5e37b9e2bc967fef7d552ccaa4736d58
xtango <xtango@users.noreply.github.com>
2021-02-16 01:03:46 -0500

    Update 151-replace-pixel-color.ts

--- daily-coding/151-replace-pixel-color.ts
+++ daily-coding/151-replace-pixel-color.ts
@@ -1,5 +1,5 @@
 /**
- *                                  #151 [Medium] - REPLACE PIXEL COLOR   **** work in progres ***
+ *                                  #151 [Medium] - REPLACE PIXEL COLOR
  * 
  * Given a 2-D matrix representing an image, a location of a pixel in the screen and a color C,
  * replace the color of the given pixel and all adjacent same colored pixels with C.

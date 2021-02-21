c2cdb58a83f5a24611ba1ea2883d215cdd2730c6
xtango <xtango@users.noreply.github.com>
2021-02-20 22:29:46 -0500

    Create rearrage-as-palindrome.ts

---
+++ daily-coding/rearrage-as-palindrome.ts
@@ -0,0 +1,48 @@
+/**
+ *                  #157 [Easy] PERMUTATION IS PALINDROME
+ *
+ * This problem was asked by Amazon.
+ * 
+ * Given a string, determine whether any permutation of it is a palindrome.
+ * For example, carrace should return true, since it can be rearranged to
+ * form racecar, which is a palindrome. daily should return false, since
+ * there's no rearrangement that can form a palindrome.
+ */
+
+/**
+ * Recursive generation of permuations of arr using Heap's algo https://en.wikipedia.org/wiki/Heap%27s_algorithm
+ */
+const heapsPermute = (arr: string[], permsAccum: string[] = [], k: number = arr.length): string[] => {
+    if (k === 1) {
+        permsAccum.push(arr.slice(0));
+    }
+
+    for (let i = 0; i < k; i++) {
+        // Recurse
+        heapsPermute(arr, permsAccum, k - 1);
+        // Swap adjacent elemement
+        k % 2
+            // Odd
+            ? [arr[0], arr[k - 1]] = [arr[k - 1], arr[0]]
+            // Even
+            : [arr[i], arr[k - 1]] = [arr[k - 1], arr[i]]
+    }
+    return permsAccum
+}
+
+const stringReverse = (str: string): string => str.split('').reverse().join("");
+
+const stringPerm = (str: string): string[] => heapsPermute(str.split('')).map(x => x.join(''));
+
+const isPalindrome = (str: string): boolean => str === stringReverse(str)
+
+const canRearrange = (str: string): boolean => stringPerm(str).find(perm => isPalindrome(perm)) !== undefined
+
+/**
+ * ASSERTIONS
+ */
+console.log(stringReverse('ab') === 'ba');
+console.log(canRearrange('cac') === true);
+console.log(canRearrange('carrace') === true);
+console.log(canRearrange('daily') === false);
+console.log(canRearrange('abc') === false)

fcda3b4e56681cbf83c656b30169a125d1a33b7f
xtango <xtango@users.noreply.github.com>
2021-02-22 18:08:06 -0500

    Create 138-min-coins.ts

---
+++ daily-coding/138-min-coins.ts
@@ -0,0 +1,12 @@
+ /**
+  *                 #138 [Hard] - MIN NUM OF COINS
+  * 
+  * This problem was asked by Google.
+  * Find the minimum number of coins required to make n cents.
+  * You can use standard American denominations, that is, 1¢, 5¢, 10¢, and 25¢.
+  * For example, given n = 16, return 3 since we can make it with a 10¢, a 5¢, and a 1¢.
+  */
+
+  const DENOMS = [1, 5, 10, 25];
+  
+  

084b4c2e4b43285d617a094f452b2c869c09c40d
xtango <xtango@users.noreply.github.com>
2021-02-21 20:04:53 -0500

    Update 158-maze-num-paths.ts

--- daily-coding/158-maze-num-paths.ts
+++ daily-coding/158-maze-num-paths.ts
@@ -1,6 +1,8 @@
 /**
  *                              #158 [Medium] - WAYS TO MOVE THROUGH MAZE
- * 
+ *
+ * This problem was asked by Slack.
+ *
  * You are given an N by M matrix of 0s and 1s. Starting from the top left corner, how many ways are there to reach the bottom right corner?
  * You can only move right and down. 0 represents an empty space while 1 represents a wall you cannot walk through.
  * For example, given the following matrix:

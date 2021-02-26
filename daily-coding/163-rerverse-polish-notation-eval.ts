e29b879466ce9dbb8238563149b489e98dca5145
xtango <xtango@users.noreply.github.com>
2021-02-26 17:29:03 -0500

    Update 163-rerverse-polish-notation-eval.ts

--- daily-coding/163-rerverse-polish-notation-eval.ts
+++ daily-coding/163-rerverse-polish-notation-eval.ts
@@ -1,38 +1,59 @@
 /**
- *            #163 [Hard] - RPN EVALUATOR
+ *            #163 [Hard] - RPN EXPRESSION EVALUATOR
  * 
  * This problem was asked by Jane Street.
-Given an arithmetic expression in Reverse Polish Notation, write a program to evaluate it.
-The expression is given as a list of numbers and operands. For example: [5, 3, '+']
-should return 5 + 3 = 8.
-For example, [15, 7, 1, 1, '+', '-', '/', 3, '*', 2, 1, 1, '+', '+', '-'] 
-should return 5, since it is equivalent to ((15 / (7 - (1 + 1))) * 3) - (2 + (1 + 1)) = 5.
+ * 
+ * Given an arithmetic expression in Reverse Polish Notation, write a program to evaluate it.
+ * The expression is given as a list of numbers and operands. 
+ * For example: [5, 3, '+'] should return 5 + 3 = 8.
+ * For example, [15, 7, 1, 1, '+', '-', '/', 3, '*', 2, 1, 1, '+', '+', '-'] 
+ * should return 5, since it is equivalent to ((15 / (7 - (1 + 1))) * 3) - (2 + (1 + 1)) = 5.
 */
 
-type Elem = number | '+' | '-' | '*';
+type Operator =  '+' | '-' | '*' | '/';
+type Elem = number | Operator;
 
-const evalExpr = (left:number, op: string, right: number): number => {
-    switch (op) {
-        case '+': return left * right;
-        case '-':return left - right;
+/**
+ * Evaluates <left number> OPERATOR <right number>
+ * If op is not recognized returns NEGATIVE_INFINITY.
+ * @example evalExpr(-5, '*', 10) returns -50
+ */
+const evalExpr = (left: number, op: Operator, right: number): number => {
+    if (op === '+') {
+        return left + right;
+    } else if (op == '-') {
+        return left - right;
+    } else if (op === '*') {
+        return left * right;
+    } else if (op === '/') {
+        return left / right;
     }
+    return Number.NEGATIVE_INFINITY;
 }
-const rpnEval = (lst: Elem[]): number | undefined => {
-    const stack: Elem[] = [];
-    lst.forEach(e => stack.unshift(e)); // unshift adds to head
-    let answer: number = undefined;
-        
-    while (stack.length > 0) {
-        const elem = stack.pop();
-        
-        if (isNaN(elem)) { // hit an operator
-            const rightNum = stack.pop();
-            const leftNum = stack.pop();
-            answer = evalExpr(rightNum,elem, leftNum);
+
+const rpnExprEval = (rpnExpression: Elem[]): number => {
+    const stack: number[] = []; // contains numbers
+    rpnExpression.forEach(elem => {
+        // Push when a number
+        if (typeof elem === 'number') {
+            stack.push(elem);
+        } else { // Pop 2 numbers when an operator is hit
+            const right = stack.pop();
+            const left = stack.pop();
+            stack.push(evalExpr(left, elem, right));
         }
-    }
-
-    return answer;
+    });
+    return stack[0];
 }
-
-rpnEval([15, 7, '+'])
+/**
+ *  ASSERTIONS
+ */
+console.log(evalExpr(-5, '*', 10) === -50);
+console.log(evalExpr(-5, '~', 10) === Number.NEGATIVE_INFINITY);
+console.log(rpnExprEval([15, 7, '+']) == 22);
+console.log(rpnExprEval([15, 7, '-']) === 8);
+console.log(rpnExprEval([14, 7, '/']) === 2);
+console.log(rpnExprEval([14, 7, '*']) === 98);
+console.log(
+    rpnExprEval([15, 7, 1, 1, '+', '-', '/', 3, '*', 2, 1, 1, '+', '+', '-']) === 5
+);

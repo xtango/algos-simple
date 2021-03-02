940b9953c75dc223e76c162b29ec888c6e9f45df
xtango <xtango@users.noreply.github.com>
2021-03-01 21:50:09 -0500

    Update 166-iterator-2d.ts

--- daily-coding/166-iterator-2d.ts
+++ daily-coding/166-iterator-2d.ts
@@ -1,20 +1,23 @@
 /**
-
-This problem was asked by Uber.
-
-Implement a 2D iterator class. It will be initialized with an array of arrays, and should implement the following methods:
-
-next(): returns the next element in the array of arrays. If there are no more elements, raise an exception.
-has_next(): returns whether or not the iterator still has elements left.
-For example, given the input [[1, 2], [3], [], [4, 5, 6]], calling next() repeatedly should output 1, 2, 3, 4, 5, 6.
-
-Do not use flatten or otherwise clone the arrays. Some of the arrays can be empty.
+ *                      #166 [Medium] - 2D ITERATOR
+ *
+ * This problem was asked by Uber.
+ *
+ * Implement a 2D iterator class. It will be initialized with an array of arrays, and should implement the following methods:
+ *
+ * next(): returns the next element in the array of arrays. If there are no more elements, raise an exception.
+ * has_next(): returns whether or not the iterator still has elements left.
+ * For example, given the input [[1, 2], [3], [], [4, 5, 6]], calling next() repeatedly should output 1, 2, 3, 4, 5, 6.
+ *
+ * Do not use flatten or otherwise clone the arrays. Some of the arrays can be empty.
 */
 
+interface RowCol { row: number, col: number }
+
 class Iterator2D {
-    curr = { row: 0, col: 0 }
-    last = { row: -1, col: -1 }
-    nonEmpty: number[][] = [];
+    current: RowCol = { row: -1, col: -1 }; // Row, col of current non-empty elem
+    last: RowCol = { row: -1, col: -1 }; // Row, col of the last non-empty elem
+    nonEmpty: number[][] = []; // References to non-empty arrays
 
     constructor(readonly input: number[][]) {
         this.nonEmpty = input.filter(r => r.length > 0);
@@ -25,24 +28,39 @@ class Iterator2D {
     }
 
     hasNext(): boolean {
-        return !(this.curr.row === this.last.row && this.curr.col === this.last.col);
+        const next = this.nextRowCol(this.current);
+        return next.row <= this.last.row && next.col <= this.last.col;
     }
 
-    next(): number | undefined {
-        if (!this.hasNext()) {
-            throw 'No more elems';
-        }
-
-        const lastColInRow = this.curr.col === this.nonEmpty[this.curr.row].length - 1;
-        if (lastColInRow) {
-            this.curr.col = 0;
-            this.curr.row++;
+    nextRowCol(curr: RowCol): RowCol {
+        let next;
+        if (curr.row === -1 && curr.col === -1) {
+            next = { row: 0, col: 0 }
         } else {
-            this.curr.col++;
+            const isLastColInRow = curr.col === this.nonEmpty[curr.row].length - 1;
+            next = isLastColInRow
+                ? { row: curr.row + 1, col: 0 }
+                : { row: curr.row, col: curr.col + 1 }
         }
-        return this.nonEmpty[this.curr.row][this.curr.col];
+        return next;
+    }
+
+    next(): number {
+        if (this.hasNext()) {
+            this.current = this.nextRowCol(this.current);
+        } else {
+            throw 'No more elems'
+        }
+        return this.nonEmpty[this.current.row][this.current.col];;
     }
 }
 
+/**
+ * ASSERTIONS
+ */
 const iter = new Iterator2D([[1, 2], [3], [], [4, 5, 6]]);
-console.log(iter.next() == 1)
+const accum = []
+while (iter.hasNext()) {
+    accum.push(iter.next());
+}
+console.log(JSON.stringify(accum) === '[1,2,3,4,5,6]');

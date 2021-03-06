8c565a1df0e33352c724e03d102e544cd02221b5
xtango <xtango@users.noreply.github.com>
2021-03-05 19:08:21 -0500

    Update 169-sort-linked-list.ts

--- daily-coding/169-sort-linked-list.ts
+++ daily-coding/169-sort-linked-list.ts
@@ -4,21 +4,24 @@
  * This problem was asked by Google.
  * 
  * Given a linked list, sort it in O(n log n) time and constant space.
- * 
  * For example, the linked list 4 -> 1 -> -3 -> 99 should become -3 -> 1 -> 4 -> 99.
  */
 
 interface LLNode { val: number, next?: LLNode }
 
-const NULL_NODE = { val: -1 }
-
 const copyList = (node: LLNode): LLNode => {
-    let copyPtr: LLNode = { val: node.val}; 
-    const copyHead = copyPtr;
+    let [copyHead, copyPtr] = [undefined, undefined];
     let sourcePtr: LLNode | undefined = node;
-    while(sourcePtr) {
-        const newNode = { val: sourcePtr.val};
-        copyPtr.next = newNode;
+    while (sourcePtr) {
+        const copy = { val: sourcePtr.val };
+        if (copyHead === undefined) {
+            copyPtr = copy;
+            copyHead = copyPtr;
+        } else {
+            copyPtr.next = copy;
+            copyPtr = copy;
+        }
+
         sourcePtr = sourcePtr.next;
     }
     return copyHead;
@@ -47,10 +50,8 @@ const merge = (left: LLNode, right: LLNode): LLNode | undefined => {
     }
 
     // Leftover elems
-    const remaining = copyList(leftPtr === undefined ? rightPtr : leftPtr);
-    console.log('rem', remaining);
-    mergedPtr.next = remaining;
-  
+    mergedPtr.next = copyList(leftPtr === undefined ? rightPtr : leftPtr);
+
     return mergedHead;
 }
 
@@ -63,8 +64,11 @@ const pretty = (head: LLNode | undefined) => {
     return str;
 }
 
-//console.log(pretty({ val: 1, next: { val: 3 } }) === '1 -> 3');
-console.log('copy', copyList({ val: 8, next: { val: 99}}))
-// console.log(
-//     merge({ val: 1, next: { val: 8 } },
-//         { val: 5, next: { val: 6 } }));
+/**
+ * ASSERTIONS
+ */
+console.log(pretty({ val: 1, next: { val: 3 } }) === '1 -> 3');
+console.log(pretty(copyList({ val: 8, next: { val: 99 } })) === '8 -> 99');
+console.log(pretty(merge(
+    { val: 1, next: { val: 8 } },
+    { val: 5, next: { val: 6 } })) === '1 -> 5 -> 6 -> 8');

11cda26a6f3780802fad7c4871051e19a0507705
xtango <xtango@users.noreply.github.com>
2021-03-12 17:06:32 -0500

    Update 177-rotate-linked-list.ts

--- daily-coding/177-rotate-linked-list.ts
+++ daily-coding/177-rotate-linked-list.ts
@@ -7,3 +7,16 @@
  * For example, given the linked list 7 -> 7 -> 3 -> 5 and k = 2, it should become 3 -> 5 -> 7 -> 7.
  * Given the linked list 1 -> 2 -> 3 -> 4 -> 5 and k = 3, it should become 3 -> 4 -> 5 -> 1 -> 2.
  */
+interface LinkedListNode { val: number, next?: LinkedListNode}
+
+const arrToLL (arr: number[]): LinkedLinstNode => {
+  const head = { val: arr[0] }
+  for (i = 1; i < arr.length: i++} {
+       head.next = { val: arr[i] }
+  }
+}
+               
+/**
+ * Rotate using slow and fast pointers.
+ */
+

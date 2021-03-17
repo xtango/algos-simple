c2eebd0bf9f151fef8d7679bf8412b5d837634f6
xtango <xtango@users.noreply.github.com>
2021-03-17 09:29:27 -0400

    Update 181-split-into-palindromes.ts

--- daily-coding/181-split-into-palindromes.ts
+++ daily-coding/181-split-into-palindromes.ts
@@ -8,7 +8,7 @@
  * Given the input string abc, return ["a", "b", "c"].
  */
 
-const isPalindrome = (word: string) => {
+const isPalindrome = (word: string): boolean => {
     for (let i = 0; i < word.length >> 1; i++) {
         if (word[i] !== word[word.length - 1 - i]) {
             return false;
@@ -17,54 +17,51 @@ const isPalindrome = (word: string) => {
     return true;
 }
 
-const splitIntoPalindromes = (strInput: string): string[] => {
-    let output: string[] = [];
-
-    /**
-     * Helper for recursion
-     */
-    const split = (str: string, depth: number = 0): string[] => {
-        let palinSets: string[][] = [];
-        console.log(`[depth: ${depth}] ${strInput}`);
-        if (depth > 1) {
-            return [];
-        }
-
-        if (strInput.length === 0) {
-            return [];
-        }
-        if (strInput.length === 1) {
-            return [strInput]
-        }
-
-
-        for (let i = 0; i < strInput.length; i++) {
-            for (let j = strInput.length; j > i; j--) {
-                const word = strInput.substring(i, j + 1);
-                const left = strInput.substring(0, i);
-                const right = strInput.substring(j + 1)
-                console.log({ left, word, right });
-                if (isPalindrome(word)) {
-                    console.log('   -> palin');
-                    const leftPalins = left.length > 0 ? splitIntoPalindromes(left, depth + 1) : [];
-                    const rightPalins = right.length > 0 ? splitIntoPalindromes(right, depth + 1) : [];
-                    console.log({ left, leftPalins, wordPalins: word, right, rightPalins });
-                    palinSets.push(leftPalins.concat(word, rightPalins));
-                    break;
-                }
-            }
-        }
-
-        console.log({palinSets});
-        // return the smallest of the sets
-        const freqs = palinSets.map(x=> { return { len: x.length, val: x}});
-        freqs.sort( (a, b) => a.len - b.len);
-        console.log('freqs', freqs)
-        return freqs[0].val;
+/**
+ * Helper for recursion
+ */
+const split = (str: string, depth: number = 0): string[] => {
+    let palinSets: string[][] = [];
+    console.log(`[depth: ${depth}] ${str}`);
+    if (depth > 1) {
+        return [];
     }
 
-    split(strInput);
-    return output;
+    if (str.length === 0) {
+        return [];
+    }
+    if (str.length === 1) {
+        return [str]
+    }
+
+    for (let i = 0; i < str.length; i++) {
+        for (let j = str.length; j > i; j--) {
+            const word = str.substring(i, j + 1);
+            const left = str.substring(0, i);
+            const right = str.substring(j + 1)
+            console.log({ left, word, right });
+            if (isPalindrome(word)) {
+                console.log('   -> palin');
+                const leftPalins = left.length > 0 ? split(left, depth + 1) : [];
+                const rightPalins = right.length > 0 ? split(right, depth + 1) : [];
+                console.log({ left, leftPalins, wordPalins: word, right, rightPalins });
+                palinSets.push(leftPalins.concat(word, rightPalins));
+                break;
+            }
+        }
+    }
+
+    console.log({ palinSets });
+    // return the smallest of the sets
+    const freqs = palinSets.map(x => { return { len: x.length, val: x } });
+    freqs.sort((a, b) => a.len - b.len);
+    console.log('freqs', freqs)
+    return freqs[0].val;
+}
+
+
+const splitIntoPalindromes = (strInput: string): string[] => {
+    return split(strInput, 0);
 }
 
 /**

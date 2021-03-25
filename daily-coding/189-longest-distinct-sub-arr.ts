fd003232dbef4320e621cbf1b1d67f5cebe46222
xtango <xtango@users.noreply.github.com>
2021-03-24 21:00:08 -0400

    Update 189-longest-distinct-sub-arr.ts

--- daily-coding/189-longest-distinct-sub-arr.ts
+++ daily-coding/189-longest-distinct-sub-arr.ts
@@ -1,5 +1,13 @@
 /**
- * #189
+ *                  #189 - LONGEST DISTINCT SUB array
+ * 
+ * This problem was asked by Google.
+ * 
+ * Given an array of elements, return the length of the 
+ * longest subarray where all its elements are distinct.
+ * 
+ * For example, given the array [5, 1, 3, 5, 2, 3, 4, 1],  return 5 
+ * as the longest subarray of distinct elements is [5, 2, 3, 4, 1]
  */
 const longestDistinctSubArray = (arr: number[]): number => {
     let [i, longest] = [0, 0];
@@ -10,10 +18,15 @@ const longestDistinctSubArray = (arr: number[]): number => {
             dict.add(arr[j]);
             j++;
         }
-        console.log('dict', dict);
+        // console.log(dict);
         longest = Math.max(longest, dict.size)
         i++;
     }
     return longest;
 }
-console.log(longestDistinctSubArray([5, 1, 5, 4]));
+
+/**
+ * ASSERTIONS
+ */
+console.log(longestDistinctSubArray([5, 1, 5, 4]) === 3);
+console.log(longestDistinctSubArray([5, 1, 3, 5, 2, 3, 4, 1]) === 5);

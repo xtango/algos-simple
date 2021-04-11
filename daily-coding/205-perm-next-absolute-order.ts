ac2859f2a853932f4215acc90f2a81fce792357d
xtango <xtango@users.noreply.github.com>
2021-04-11 14:36:32 -0400

    Update 205-perm-next-absolute-order.ts

--- daily-coding/205-perm-next-absolute-order.ts
+++ daily-coding/205-perm-next-absolute-order.ts
@@ -1,5 +1,5 @@
 /**
- *                #205 [Easy] - NEXT PERMUTATION OF NUM
+ *                   #205 [Easy] - NEX PERMUTATION IN ABSOLUTE ORDER
  *
  * Good morning! Here's your coding interview problem for today.
  * 
@@ -8,19 +8,16 @@
  * For example, given 48975, the next permutation would be 49578.
  */
 
-
-const numToArr = (num: Number): number[] => num.toString().split('').map(x=> parseInt(x));
-
 /**
- * From right to left we find the digit that's not desc sorted. This 
+ * From right to left we find the digit that's not descending-sorted. This 
  * is the index of the candidate digit to replace to possibly make the perm larger.
  * 
  * @example findTailIndex(4321) -> 0 as there's no larger perm
- * @example findTailIndex(4312) -> 3 // as the number 2 is a replacement candidate. Replacing 2 with 1 gives 4321
+ * @example findTailIndex(4312) -> 3 as the number 2 is a replacement candidate. Replacing 2 with 1 gives 4321
  */
-const findTailIndex = (numStr: string): number => {
+const findStartIndex = (numStr: string): number => {
     let i = numStr.length - 1;
-    while(i > 0 &&  numStr[i-1] > numStr[i]) {
+    while (i > 0 && numStr[i - 1] > numStr[i]) {
         i--;
     }
     return i;
@@ -28,15 +25,34 @@ const findTailIndex = (numStr: string): number => {
 
 const nextLargerPerm = (num: number): number => {
     const numStr = num.toString();
-    const tailIdx = findTailIndex(numStr);
-    if (tailIdx === 0) {
+    const startIdx = findStartIndex(numStr);
+    if (startIdx === 0) {
         return num;
     }
-    
-    // For '4312' tailIdx is 3, pointing to '2'. rhs = 12 (include the left char)
-    if (tailIdx > 0) {
-        const rhs = numStr.substring(tailIdx -1)
 
+    // @example '43120'
+    //          findStartIndex('43120')                      -> 3
+    //          head, tail:                                  -> '1', '20'
+    //          Replace head w/ smallest digit > '1', 2      -> '2' + ...
+    //          Next place '1' to right of smallest digit, 0 -> '201'
+    //          '43' concat '201'                            -> '43201'
+    if (startIdx > 0) {
+        const [head, tail] = [numStr[startIdx - 1], numStr.substring(startIdx)];
+        let [i, min, minIdx, minGTHead, minGTHeadIdx] = [0, tail[0], 0, tail[0], 0];
+        while (i < tail.length) {
+            if (tail[i] > head && tail[i] < minGTHead) {
+                minGTHead = tail[i];
+                minGTHeadIdx = i;
+            }
+            if (tail[i] < min) {
+                min = tail[i];
+                minIdx = i;
+            }
+            i++;
+        }
+        const nextPerm = numStr.substring(0, startIdx - 1) + tail[minGTHeadIdx] + tail.substring(minIdx) + head;
+        console.log({ num, head, tail, minGTHead, minGTHeadIdx, nextPerm });
+        return parseInt(nextPerm);
     }
     return num;
 }
@@ -44,7 +60,6 @@ const nextLargerPerm = (num: number): number => {
 /**
  * ASSERTIONS
  */
-console.log(numToArr(1234).join(' ') === '1 2 3 4')
-console.log(findTailIndex('4321') === 0);
-console.log(findTailIndex('4312') === 3);
-//console.log(nextLargerPerm(NUM2));
+console.log(findStartIndex('4321') === 0);
+console.log(findStartIndex('4312') === 3);
+console.log(nextLargerPerm(43120) === 43201);

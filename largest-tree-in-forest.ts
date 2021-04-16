4a36b3abb9876f107fb8303daef082537ccbce6b
xtango <xtango@users.noreply.github.com>
2021-04-16 18:40:31 -0400

    Create largest-tree-in-forest.ts

---
+++ largest-tree-in-forest.ts
@@ -0,0 +1,96 @@
+/**
+ * Index of largest tree in forest
+ */
+
+type ChildToParent = number[];
+interface TreeNode { id: number, children: TreeNode[] }
+
+type ParentToChildDict = { [key: number]: number[] };
+
+interface Forest {
+    parentToChildDict: ParentToChildDict;
+    roots: number[]; // indices of root nodes
+}
+
+/**
+ * Conversts child-parent pairs to parent-children.
+ */
+const toForest = (childToParentList: ChildToParent[]): Forest => {
+    const parentToChildDict: ParentToChildDict = {};
+    const children = childToParentList.map(x => x[0]);
+    const rootSet = new Set<number>();
+
+    childToParentList.forEach(childToParent => {
+        const [child, parent] = [childToParent[0], childToParent[1]];
+        if (parentToChildDict[parent] === undefined) {
+            parentToChildDict[parent] = [];
+        }
+        parentToChildDict[parent].push(child);
+        // Root when parent not in children list
+        if (!children.includes(parent)) {
+            rootSet.add(parent);
+        }
+    });
+
+    const forest = {
+        parentToChildDict,
+        roots: [...rootSet]
+    };
+    ///console.log(forest);
+    return forest;
+}
+
+const treeSize = (forest: Forest, rootIndex: number): number {
+    //console.log('treesize forst', forest);
+    let count = 1;
+    const q: number[] = [rootIndex];
+    while (q.length > 0) {
+        const idx = q.pop();
+        const children = forest.parentToChildDict[idx];
+        if (children !== undefined) {
+            children.forEach(child => {
+                q.push(child)
+                count++;
+            });
+        }
+    }
+    return count;
+}
+
+const largestIdx = (childToParentList: ChildToParent[]): number => {
+    const forest = toForest(childToParentList);
+    let [maxSize, maxSizeRootIdx] = [0, 0];
+    forest.roots.forEach(idx => {
+        const size = treeSize(forest, idx);
+        if (size > maxSize) {
+            [maxSize, maxSizeRootIdx] = [size, idx];
+        }
+    });
+    return maxSizeRootIdx;
+}
+
+/**
+ *          1
+ *         2  3
+ */
+const CHILD_PARENT_1 = [
+    [2, 1],
+    [3, 1]]
+console.log(toForest(CHILD_PARENT_1).roots.length === 1);
+console.log(treeSize(toForest(CHILD_PARENT_1), 1) == 3);
+console.log(largestIdx(CHILD_PARENT_1) === 1);
+
+/**
+ *      1       4
+ *     2        5
+ *    3
+ */
+const CHILD_PARENT_2 = [
+    [2, 1],
+    [3, 2],
+    [5, 4]]
+console.log(toForest(CHILD_PARENT_2)
+    .roots
+    .length === 2);
+console.log(treeSize(toForest(CHILD_PARENT_2), 4) === 2);
+console.log(largestIdx(CHILD_PARENT_2) === 1);

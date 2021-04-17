853997e50879e0174dee37b53c1e12298c79eccd
xtango <xtango@users.noreply.github.com>
2021-04-17 10:20:29 -0400

    Update tick-tac-toe-minimax.ts

--- puzzle/tick-tac-toe-minimax.ts
+++ puzzle/tick-tac-toe-minimax.ts
@@ -1,5 +1,5 @@
 /**
- * TIC-TAC-TOE - MINIMAX
+ *                                  TIC-TAC-TOE - MINIMAX
  * 
  *      "The player who succeeds in placing three of their marks in a horizontal, vertical,
  *      or diagonal row is the winner. It is a solved game with a forced draw assuming best

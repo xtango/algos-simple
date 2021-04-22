69d8e6f5c82b82aabed659861aee86f8e392c6d1
xtango <xtango@users.noreply.github.com>
2021-04-22 16:56:27 -0400

    Update 216-roman-to-decimal.ts

--- daily-coding/216-roman-to-decimal.ts
+++ daily-coding/216-roman-to-decimal.ts
@@ -2,12 +2,23 @@
  *                              Problem #216 [Medium] - ROMAN TO DECIMAL
  * 
  * This problem was asked by Facebook.
+ * 
+ * Given a number in Roman numeral format, convert it to decimal.
+ * The values of Roman numerals are as follows:
+ * {
+ *     'M': 1000,
+ *     'D': 500,
+ *     'C': 100,
+ *     'L': 50,
+ *     'X': 10,
+ *     'V': 5,
+ *     'I': 1
+ * }
+ * In addition, note that the Roman numeral system uses subtractive notation for numbers such as IV and XL.
+ * For the input XIV, for instance, you should return 14.
+ */
 
-Given a number in Roman numeral format, convert it to decimal.
-
-The values of Roman numerals are as follows:
-
-{
+const VALUES: { [numeral: string]: number } = {
     'M': 1000,
     'D': 500,
     'C': 100,
@@ -15,8 +26,36 @@ The values of Roman numerals are as follows:
     'X': 10,
     'V': 5,
     'I': 1
-}
-In addition, note that the Roman numeral system uses subtractive notation for numbers such as IV and XL.
+};
 
-For the input XIV, for instance, you should return 14.
+const romanToDec = (str: string): number => {
+    let sum = VALUES[str[0]];
+    for (let i = 1; i < str.length; i++) {
+        const [prev, curr] = [VALUES[str[i - 1]], VALUES[str[i]]];
+        sum += prev >= curr
+            ? curr
+            : curr - prev - prev; // subtraction rule
+    }
+    return sum;
+}
+
+/**
+ * ASSERTIONS From wikipedia
  */
+Object.entries({
+    'VI': 6,
+    'XXXIX': 39,
+    'CCXLVI': 246,
+    'MMCDXXI': 2421,
+    //
+    'CLX': 160,
+    'CCVII': 207,
+    'MIX': 1009,
+    'MLXVI': 1066,
+    //
+    'MDCCLXXVI': 1776, // (the date written on the book held by the Statue of Liberty)
+    'MCMXVIII': 1918, // (the first year of the Spanish flu pandemic)
+    'MCMLIV': 1954, // (as in the trailer for the movie The Last Time I Saw Paris)
+    'MMXIV': 2014 // (the year of the games of the XXII (22nd) Olympic Winter Games (in Sochi, Russia))
+}
+).forEach(([key, val]) => console.log(key, val, romanToDec(key) === val));

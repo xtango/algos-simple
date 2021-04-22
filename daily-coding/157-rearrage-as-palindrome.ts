94ff50d5a4894cf8042aaf04f0a859aa2349b310
xtango <xtango@users.noreply.github.com>
2021-04-22 14:16:22 -0400

    Update 157-rearrage-as-palindrome.ts

--- daily-coding/157-rearrage-as-palindrome.ts
+++ daily-coding/157-rearrage-as-palindrome.ts
@@ -10,17 +10,29 @@
  */
 
 /**
- * Recursive generation of permuations of arr using Heap's algo https://en.wikipedia.org/wiki/Heap%27s_algorithm
+ * Recursive generation of permuations of arr using Heap's algo
+ * @see https://en.wikipedia.org/wiki/Heap%27s_algorithm
+ * @example heapsPermute(['a', 'b', 'c']) returns
+ *          [["a", "b", "c"],
+ *           ["b", "a", "c"],
+ *           ["c", "a", "b"],
+ *           ["a", "c", "b"],
+ *           ["b", "c", "a"],
+ *           ["c", "b", "a"]] 
  */
-const heapsPermute = (arr: string[], permsAccum: string[] = [], k: number = arr.length): string[] => {
+const heapsPermute = (
+    arr: string[],
+    permsAccum: string[][] = [],
+    k: number = arr.length
+): string[][] => {
     if (k === 1) {
-        permsAccum.push(arr.slice(0));
+        permsAccum.push([...arr]); // Push copy of arr
     }
 
     for (let i = 0; i < k; i++) {
         // Recurse
         heapsPermute(arr, permsAccum, k - 1);
-        // Swap adjacent elemement
+        // Swap choice dependent on parity of k (even or odd)
         k % 2
             // Odd
             ? [arr[0], arr[k - 1]] = [arr[k - 1], arr[0]]
@@ -41,8 +53,10 @@ const canRearrange = (str: string): boolean => stringPerm(str).find(perm => isPa
 /**
  * ASSERTIONS
  */
+console.log(stringPerm('abc').sort().join(' ') === 'abc acb bac bca cab cba');
+console.log(stringPerm('').join(' ') === '');
 console.log(stringReverse('ab') === 'ba');
 console.log(canRearrange('cac') === true);
 console.log(canRearrange('carrace') === true);
 console.log(canRearrange('daily') === false);
-console.log(canRearrange('abc') === false)
+console.log(canRearrange('abc') === false);

9b88bdb8c20499a2d0f3cba074345c1c83a1d7c6
xtango <xtango@users.noreply.github.com>
2021-04-22 15:02:48 -0400

    Update 217-smallest-sparse-number.ts

--- daily-coding/217-smallest-sparse-number.ts
+++ daily-coding/217-smallest-sparse-number.ts
@@ -1,11 +1,36 @@
 /**
-              Problem #217 [Hard] - 
+ *               Problem #217 [Hard] - SMALLEST SPARSE NUMBER
+ * 
+ * This problem was asked by Oracle.
+ * 
+ * We say a number is sparse if there are no adjacent ones in its binary representation.
+ * For example, 21 (10101) is sparse, but 22 (10110) is not.
+ * For a given input N, find the smallest sparse number greater than or equal to N.
+ * Do this in faster than O(N log N) time.
+ */
 
-This problem was asked by Oracle.
+/**
+ * Works for positive and negative numbers
+ * @see https://stackoverflow.com/questions/9939760/how-do-i-convert-an-integer-to-binary-in-javascript
+ */
+const decimalToBin = (n: number): string => (n >>> 0).toString(2);
 
-We say a number is sparse if there are no adjacent ones in its binary representation.
-For example, 21 (10101) is sparse, but 22 (10110) is not.
-For a given input N, find the smallest sparse number greater than or equal to N.
+const isSparse = (str: string): boolean => !str.includes('11');
 
-Do this in faster than O(N log N) time.
-*/
+/**
+ * Non optimal, O(N log N) solution
+ */
+const nextSparse = (n: number): number => {
+    let smallest = n + 1;
+    while (!isSparse(decimalToBin(smallest))) {
+        smallest++;
+    }
+    return smallest;
+}
+
+/**
+ * ASSERTIONS
+ */
+console.log(isSparse('10101'));
+console.log(!isSparse('10110'));
+console.log(nextSparse(21) === 32); // 100000

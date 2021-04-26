10390151ea354ab121a1979d4f612b6a122a4b2f
xtango <xtango@users.noreply.github.com>
2021-04-26 19:24:19 -0400

    Update 221-sevenish.ts

--- daily-coding/221-sevenish.ts
+++ daily-coding/221-sevenish.ts
@@ -9,29 +9,49 @@
 
 /**
  * i       :    0         1        2        3        4       5         6
- * Pow(7^x):  7^0=1    7^1=7             7^2=49            7^3=343       
- * Sums    :   1                1+7=8             8+49=57           57+343
+ * Pow(7^x):  7^0=1    7^1=7             7^2=49            
+ * Sums    :   1                1+7=8               1+49=50  7+49=56  1+7+49=57
  */
-const sevenish = (n: number): number => {
-    let [power, sum, x, sevenishVal] = [0, 0, 0, 0];
-    for (let i = 0; i <= n; i++) {
-        if (i % 2 === 0) { // even 
-            power = Math.pow(7, x);
-            //console.log('power', power);
-            sevenishVal = power;
-            x++;
-        } else {
-            sum += power;
-            //console.log('sum', sum);
-            sevenishVal = sum;
+const genPower7s = (n: number) => [...Array(n).keys()].map(x => Math.pow(7, x))
+
+/**
+ * Returns the powerset of arr
+ */
+const subsets = (arr: number[]): number[][] => {
+    const subs = [[]];
+
+    // For 1, 2, 3
+    // Start with subs = []            []
+    // concat 1 to each subset      -> [], [1]
+    // concat 2 to each subset      -> [], [1], [2], [1, 2]
+    // concat 3 to each subset      -> [], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2 ,3]
+    for (const el of arr) {
+        const last = subs.length - 1;
+        for (let i = 0; i <= last; i++) {
+            subs.push([...subs[i], el]);
         }
     }
 
-    return sevenishVal;
+    return subs;
 }
+
+/**
+ * Non-performant solutions
+ */
+const sevenish = (n: number): number => {
+    const power7s = genPower7s(n);
+    const power7Subsets = subsets(power7s);
+    const sums = power7Subsets.map(s => s.reduce((accum, x) => accum + x, 0));
+    return sums[n];
+}
+
 /**
  * ASSERTIONS
  */
-console.log([1, 2, 3, 4, 5, 6, 7]
-    .map(x => sevenish(x))
-    .join(',') === '1,7,8,49,57,343,400')
+
+// First 10 sevenish nums
+console.log(
+    [...Array(10).keys()]
+        .map(x => sevenish(x + 1))
+        .join(',') === '1,7,8,49,50,56,57,343,344,350'
+);

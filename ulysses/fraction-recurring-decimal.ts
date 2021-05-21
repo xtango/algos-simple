98fcc96ac8edc0b25012c69be01ef09b366cb02c
xtango <xtango@users.noreply.github.com>
2021-05-21 16:43:27 -0400

    Create fraction-recurring-decimal.ts

---
+++ ulysses/fraction-recurring-decimal.ts
@@ -0,0 +1,52 @@
+/**
+ *              RECURRING DECIMAL
+ * 
+ * Retuns the string represenation of a fratction.
+ * @example 1/2 returns '1.5'
+ *                                           _
+ * @example 1/3 returns 1.(3) to represent 1.3
+ *                                                  ______ 
+ * @example 4/7 returns '0.(571428)' representing 0.571428
+ *             
+ *      '. 5   7   1   4   2   8 ...'   <----str
+ * 
+ *            5   1   3   2   6   4     <--- rem is carried over
+ * 4   4 . 0   0   0   0   0   0   0   
+ * - = -----------------------------
+ * 7               7
+ */
+
+const integralPart = (numerator: number, denominator: number): string => 
+  Math.floor(numerator / denominator).toString();
+
+const fractionalPart = (numerator: number, denominator: number): string => {
+    const MAX_ITER = 10; // Circuit breaker
+    let str = '';
+    let i = 0;
+    const carries: { [key: number]: boolean } = [];
+    let rem = numerator % denominator;
+    while (rem !== 0 && i < MAX_ITER) {    // i:  0        1           2
+        let x = rem * 10;                  // 40        -> 50       -> 10 
+        const divisor = x / denominator;   // 40/7=5    -> 50/7=7   -> 10/7=1
+        rem = x % denominator;             // 40%7=5    -> 50%7=1   -> '10%7'=3
+
+        // Hit a repeating decimal pattern when the carry has alredy occurred
+        // the carry over has already occured
+        if (carries[rem]) {
+            return '(' + str + ')';
+        } else {
+            str += Math.floor(divisor);     // '.5'      -> '.57'    -> '.571'
+            carries[rem] = true;
+        }
+
+        i++;
+    }
+    return str;
+}
+
+const recurringDecFraction = (numerator: number, denominator: number) => {
+    const integral = integralPart(numerator, denominator);
+    return (numerator % denominator === 0) ? integral : integral + fractionalPart(numerator, denominator);
+}
+
+console.log(recurringDecFraction(4, 7));

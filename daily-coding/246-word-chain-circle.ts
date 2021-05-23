2c363e996792d9dec87e6fd879c3607869c4733e
xtango <xtango@users.noreply.github.com>
2021-05-23 01:36:39 -0400

    Update 246-word-chain-circle.ts

--- daily-coding/246-word-chain-circle.ts
+++ daily-coding/246-word-chain-circle.ts
@@ -13,31 +13,44 @@
  */
 
 const neighbors = (list: string[], word: string): string[] =>
-    list.filter(x => x[0] === word[word.length - 1])
+    list.filter(x => x[0] === word[word.length - 1]); //  && x !== word);
 
 const canFormChainCircle = (wordList: string[]): boolean => {
     const MAX_DEPTH = 10;
+    const visited: string[] = [];
 
     const bfs = (q: string[], depth: number): boolean => {
+        console.log(`[${depth}] q: ${JSON.stringify(q)}`);
         if (q.length === 0 || depth > MAX_DEPTH) {
             return false;
         }
-
-        console.log(`[${depth}] ${q.join(' ')}`);
-
+        
         // Dequeue head
-        const word = q.shift();                    // word=chair, q=[] -> racket, []       ->  touch, [tunic]
-        console.log('word:', word);
+        const word = q.shift();                              // word=chair, q=[] -> racket, []       ->  touch, [tunic]
+        visited.push(word);
+        console.log('\tword:', word);
 
-        const adjList = neighbors(wordList, word); // adjs=[racket]    -> [touch, tunic]   ->  [height]
+        const adjList = neighbors(wordList, word);
+            // .filter(x => !visited.includes(x)); // adjs=[racket]    -> [touch, tunic]   ->  [height]
+        if (adjList.includes(wordList[0])) {
+            return true;
+        }
         q = q.concat(adjList);
-        console.log('word, adjList', word, adjList);
+        console.log('\tadjList:', JSON.stringify(adjList));
         return bfs(q, depth + 1);
     }
 
-    console.log(wordList);
+    console.log('wordList:', JSON.stringify(wordList));
     return bfs([wordList[0]], 0);
 }
-const WORDS = ['chair', 'height', 'racket', 'touch', 'tunic'];
-console.log(JSON.stringify(neighbors(WORDS, 'height')) === '["touch","tunic"]');
-console.log(canFormChainCircle(WORDS));
+
+/**
+ * ASSERTION
+ */
+const WORDS1 = ['chair', 'height', 'racket', 'touch', 'tunic'];
+console.log(JSON.stringify(neighbors(WORDS1, 'height')) === '["touch","tunic"]');
+console.log(canFormChainCircle(WORDS1));
+
+// Test unchaninable
+// const WORDS2 = ['chair', 'height', 'racket', 'touch'];
+// console.log(canFormChainCircle(WORDS2));

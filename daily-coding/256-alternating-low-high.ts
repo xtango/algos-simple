eb1688d34d9ce1c4d21cb0a1928bcbb9cba99296
xtango <xtango@users.noreply.github.com>
2021-05-31 19:15:49 -0400

    Update 256-alternating-low-high.ts

--- daily-coding/256-alternating-low-high.ts
+++ daily-coding/256-alternating-low-high.ts
@@ -19,7 +19,34 @@ const llToArray = (head: LLNode): number[] => {
     }
     return arr;
 }
+/**
+ * Returns a new linked list in alternating high-low form
+ * 
+ * i:    0   1   2   3   4          0   1   2   3   4
+ * 
+ * val:  1   2   3   4   5      =>  1   3   2   5   4
+ *           |___|   |___|   
+ */
+const alternate = (head: LLNode): LLNode => {
+    const arr = llToArray(head).sort();
+    let node: LLNode = { val: arr[0] };
+    const rearrangedHead = node;
+    for (let i = 1; i < arr.length - 1; i++) {
+        // Swap every odd i'th elem with elem to the right
+        if (i % 2 === 1) {
+            node.next = {
+                val: arr[i + 1],
+                next: { val: arr[i] }
+            };
+            node = node.next.next;
+        }
+    }
+    return rearrangedHead;
+}
 
+/**
+ * ASSERTIONS
+ */
 const LL1 = {
     val: 1,
     next: {
@@ -32,42 +59,7 @@ const LL1 = {
             }
         }
     }
-};
-
-/**
- * Swaps 
- * i:0    1   2   3   4
- *   1    2   3   4   5      =>  1   3   2   5   4
- *        |___|   |___|   
- */
-const alternate = (head: LLNode): LLNode => {
-    const arr = llToArray(head).sort();
-    for (let i = 1; i < arr.length -1; i++) {
-       // Swap every odd i'th elem and with elem to the right
-       if (i % 2 === 1) {
-           console.log(arr[i+1]);
-           console.log(arr[i]);
-       }
-    }
-}
-
-/**
- * ASSERTIONS
- */
-
-const LL1 = {
-    val: 1,
-    next: {
-        val: 2,
-        next: {
-            val: 3,
-            next: {
-                val: 4,
-                next: { val: 5}
-            }
-        }
-    }
 
 }
 console.log(JSON.stringify(llToArray(LL1)) === '[1,2,3,4,5]');
-console.log(JSON.stringify(llToArray(LL1)) === '[1,2,3,4,5]');
+console.log(JSON.stringify(llToArray(alternate(LL1))) === '[1,3,2,5,4]');

3c0ffd85a966731af9e633fb96f4092335d7f7d3
xtango <xtango@users.noreply.github.com>
2021-08-21 18:41:53 -0400

    Update 334-reach-24.ts

--- daily-coding/334-reach-24.ts
+++ daily-coding/334-reach-24.ts
@@ -1,5 +1,5 @@
 /**
- * Problem #334 [Easy]
+ *              Problem #334 [Easy] - Reach 24 Game
  * 
  * This problem was asked by Twitter.
 
@@ -13,10 +13,42 @@ For example, given the input [5, 2, 7, 8], you should return True, since (5 * 2 
 Write a function that plays the 24 game.
 */
 
-const calc = (a: number, b: number) => [a + b, a - b, a * b, a / b, b /a];
+/**
+ * APPROACH
+ *  number of permutations that would have to be evaluated for a given round of 24, where 4 numbers are given and 4 ops are allowed.
+ * 
+We can arrange the numbers in 4! = 24 ways
+We can arrange the three operators in 4p3 ways (ignoring the fact that addition and multiplication are commutative), which also happens to be 4!, which also happens to be 24
+
+Edit: IIRC operations can be repeated, so there are actually 4**3, or 64 "op permutations". This brings us to 1560 permutations to check, which is still reasonable.
+
+The total number of possible "24 equations" for a given selection of four random numbers is 24 * 24, which equals 576. Lucky for us, modern PCs can evaluate 576 arithmetic equations in just a few microseconds.
+ */
+
+/**
+ * abc => abc, acb, 
+ */
+const permutationsWithRepeats = (input: string) => {
+    const out: string[] = [];
+
+    // Fix the index char and recurse the subsequent chars
+    const recurHelper = (index: number) => {
+        let arr: string[] = new Array(input.length);
+        for (let i = 0; i < input.length; i++) {
+            // Fix the char at index
+            arr[index]=input[i];
+            // Reached end when last char. Otherwise recurse.
+            if (i === input.length- 1) {
+                out.push(arr.join('')); // convert to str
+            } else {
+                recurHelper(index + 1);
+            }
+        }
+    }
+    recurHelper(0);
+}
 
 const canReach24 = (input: number[]): boolean => {
-    
     const dfs = (nums: number[]) => {
         
     }

3bcaa231eb54e5aa28cd888d608cc67645de6535
xtango <xtango@users.noreply.github.com>
2021-09-06 19:17:40 -0400

    Update 348-ternary-search-tree.ts

--- daily-coding/348-ternary-search-tree.ts
+++ daily-coding/348-ternary-search-tree.ts
@@ -3,7 +3,8 @@
  * 
  * This problem was asked by Zillow.
 
-A ternary search tree is a trie-like data structure where each node may have up to three children. Here is an example which represents the words code, cob, be, ax, war, and we.
+A ternary search tree is a trie-like data structure where each node may have up to three children.
+Here is an example which represents the words code, cob, be, ax, war, and we.
 
        c
     /  |  \
@@ -17,7 +18,8 @@ The tree is structured according to the following rules:
 left child nodes link to words lexicographically earlier than the parent prefix
 right child nodes link to words lexicographically later than the parent prefix
 middle child nodes continue the current word
-For instance, since code is the first word inserted in the tree, and cob lexicographically precedes cod, cob is represented as a left child extending from cod.
+For instance, since code is the first word inserted in the tree, and cob lexicographically 
+precedes cod, cob is represented as a left child extending from cod.
 
 Implement insertion and search functions for a ternary search tree.
  */
@@ -25,7 +27,7 @@ interface TSTNode {
     data: string;
     left?: TSTNode;
     mid?: TSTNode;
-    right?:TSTNode;
+    right?: TSTNode;
 }
 
 const newTSTNode = (char: string): TSTNode => {
@@ -38,10 +40,44 @@ const newTSTNode = (char: string): TSTNode => {
 }
 
 /**
- * In-place insertion of root intot the ternary specified by root.
+ * Create blank tree
  */
-const tstInsert = (root: TSTNode, node: TSTNode): void => {
-    if (!root) {
-
+const tstMakeTree = (): { root: TSTNode | undefined } => {
+    return {
+        root: undefined
     }
 }
+
+/**
+ * In-place insertion of root in the ternary tree
+ */
+const tstInsert = (tree, char: string) => {
+    const insertNode = (node: TSTNode | undefined) => {
+        console.log('insertNode', node, char);
+        if (node === undefined) {
+            node = newTSTNode(char);
+        } else {
+            if (char < node.data) {
+                insertNode(node.left);
+            } else if (char === node.data) {
+                insertNode(node.mid);
+            } else {
+                insertNode(node.right);
+            }
+        }
+    }
+
+    if (tree.root === undefined) {
+        tree.root = newTSTNode(char);
+    } else {
+        insertNode(tree.root);
+    }
+}
+
+/**
+ * ASSERTIONS
+ */
+const words = ['code', 'cob', 'be', 'ax', 'war', 'we']
+const tree = tstMakeTree();
+words.forEach(word => word.split('').forEach(c => tstInsert(tree, c)))
+console.log('tern tree', tree); ''

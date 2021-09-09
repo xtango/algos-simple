7561a03a92d203e8f2c85521b4dea32b04f439b7
xtango <xtango@users.noreply.github.com>
2021-09-09 18:58:04 -0400

    Update lru-cache.ts

--- lru-cache.ts
+++ lru-cache.ts
@@ -1,14 +1,19 @@
 /**
  *          LEAST RECENTLY USED CACHE
  * 
- * Approach for O(1) for both set() and get() we use 2 data 
- * structures, a hashmap and a doubly-linked list.
+ * To get O(1) performance for both lookups and insertions we use
+ * 2 data structures, a hashmap and a doubly-linked list.
  * 
- * hmap  --points to-->        DLL (head is most recent, tail the oldest)
- * ------------------------    ----------------------------------------------
- * 'x'                         h <-> x <-> a
- * 'a'
- * 'h'
+ * Cache(hashmap)           Recency (doubly-linked) list
+ * 
+ *                          (head points to most recent)
+ *                          V     
+ * 'h'----------------------|
+ *                          'h' <-> 'x' <-> 'a'
+ * 'x'-------------------------------|       |
+ *                                           |
+ * 'a'---------------------------------------
+ * 
  */
 
 interface LRUNode<T> { prev: LRUNode<T> | undefined, val: T, next: LRUNode<T> | undefined }
@@ -26,44 +31,53 @@ class DLL<T> {
             this.tail = node;
         } else {
             this.head.prev = node;
-            node.next = this.dll.head;
+            node.next = this.head;
             this.head = node;
         }
     }
 }
 
 class LRUCache<T> {
-    hmap: HashMap<T> = {};
-    dll: DLL<T> = new DLL<T>();
+    cache = new Map();
+    recencyList: DLL<T> = new DLL<T>();
 
     constructor(readonly n: number) { }
 
-    /**
-     * Sets key in cache. If miss sets a new node.
-     * When > n items the least recently used item is evicted.
-     */
-    set(key: string) {
-        const found = this.get(key);
-        if (found) {
-            this.dll.insertAtHead(found);
-        } else {
-            const newNode: LRUNode<T> = { val: key }
-            this.dll.insertAtHead(newNode);
-            this.hmap[key] = newNode;
-        }
-        return this;
-    }
+    // /**
+    //  * Sets key in cache. If cache-miss sets a new node.
+    //  * When n items reached, evicts the least recently used node.
+    //  */
+    // set(key: string, value: T) {
+    //         const newNode: LRUNode<T> = { prev: undefined, val: value, next: undefined};
+    //         this.recencyList.insertAtHead(newNode);
+    //         this.cache[key] = newNode;
+        
+    //     return this;
+    // }
 
     get(key: string) {
-        return this.hmap[key];
+        const found = this.cache.get(key);
+        if (found) {
+            this.recencyList.insertAtHead(found);
+        }
     }
 
-    
+    pretty(): string {
+        let node = this.recencyList.head;
+        const arr = []
+        while (node) {
+            arr.push(node);
+            node = node.next;
+        }
+        return arr.map(x => x.val).join('-')
+    }
+
+
 }
 
 /**
  * ASSERTIONS
  */
 const lru = new LRUCache<string>(3);
-lru.set('x').set('a');
-console.log(lru.dll);
+lru.set('a').set('x').set('h');
+console.log(lru.pretty() === 'h-x-a');

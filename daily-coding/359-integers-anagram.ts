4e657ae727344008d8ec8bf689d37993e2477a3b
xtango <xtango@users.noreply.github.com>
2021-09-17 09:13:53 -0400

    Update 359-integers-anagram.ts

--- daily-coding/359-integers-anagram.ts
+++ daily-coding/359-integers-anagram.ts
@@ -6,9 +6,9 @@
  * You are given a string formed by concatenating several words
  * corresponding to the integers zero through nine and then anagramming.
  * For example, the input could be 'niesevehrtfeev', which is an anagram
- * of 'threefiveseven'. Note that there can be multiple instances of each integer.
- * Given this string, return the original integers in sorted order. 
- * In the example above, this would be 357.
+ * of 'threefiveseven'. Note that there can be multiple instances of 
+ * each integer. Given this string, return the original integers in sorted
+ * order. In the example above, this would be 357.
  */
 
 /**
@@ -27,7 +27,7 @@ const charFrequency = (word: string): number[] => {
 }
 
 /**
- * Returns a new array of a[i] - b[i]. Assumes a and b equal in length.
+ * Returns a new array of a[i] - b[i], where a and b are frequency arrays.
  */
 const subtractFrequency = (a: number[], b: number[]): number[] => {
     const clone = [...a];

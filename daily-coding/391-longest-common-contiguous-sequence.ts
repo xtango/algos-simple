96a73ddd4e369ff702a8f692e566aef9fe2dfd84
xtango <xtango@users.noreply.github.com>
2021-10-18 21:20:58 -0400

    Update 391-longest-common-contiguous-sequence.ts

--- daily-coding/391-longest-common-contiguous-sequence.ts
+++ daily-coding/391-longest-common-contiguous-sequence.ts
@@ -27,38 +27,34 @@
 type LongestMatrix = number[][];
 
 /**
+ * Returns a blank matrix for memoization. Holds the longest common subsequence of the prefixes S[0..i] and T[0..j] 
+ * ending at position S[i], T[j], respectively.  In other words, the length of the longest found until (i, j).
+ * 
+ * For the given example:
+ *         /home     /red      /login    /user     /one      /pink     
+ * /home     1         0         0         0         0         0         
+ * /register 0         0         0         0         0         0         
+ * /login    0         0         1         0         0         0         
+ * /user     0         0         0         2         0         0         
+ * /one      0         0         0         0         3         0         
+ * /two      0         0         0         0         0         0   
+ */
+const newLongestMemo = (numRows: number, numCols: number): LongestMatrix => [...Array(numRows)]
+    .map(r => Array(numCols).fill(''));
+
+/**
  * Finds the longest contiguous sequence of words that appear on both S and T with dynamic programming.
  * 
  * @see https://en.wikipedia.org/wiki/Longest_common_substring_problem
  */
-const lcContigSeq = (S: string[], T: string[]): string[] => {
-    /**
-     *  Longest memo: longest common subsequence of the prefixes S[0..i] and T[0..j] 
-     *  ending at position S[i], T[j], respectively.
-     *  In other words, the length of the longest found until (i, j).
-     * 
-     *  For the given example:
-     *         /home     /red      /login    /user     /one      /pink     
-     * /home     1         0         0         0         0         0         
-     * /register 0         0         0         0         0         0         
-     * /login    0         0         1         0         0         0         
-     * /user     0         0         0         2         0         0         
-     * /one      0         0         0         0         3         0         
-     * /two      0         0         0         0         0         0   
-     */
-    const L: LongestMatrix = [...Array(S.length)].map(r => Array(T.length).fill(''));
+const longestCommonContiguousSeq = (S: string[], T: string[]): string[][] => {
+    const L: LongestMatrix = newLongestMemo(S.length, T.length)
 
     // Length of the longest common substring found so far.
     let longestFound: number = 0;
 
-    // TODO!
-    // The set of strings which are of length z. 
-    // The set ret can be saved efficiently by just storing the index i,
-    // which is the last character of the longest common substring (of size z) 
-    // instead of S[i-z+1..i].
-    // Thus all the longest common substrings would be, for each i in ret,
-    //  S[(ret[i]-z)..(ret[i])].
-    const ret: string[] = [];
+    // The set of strings which are of length longestFound. 
+    let ret: string[][] = [];
 
     for (let i = 0; i < S.length; i++) { //    iter 1->S[0]: home...
         for (let j = 0; j < T.length; j++) {//       iter 1 -> T[0]: home, T[1]: red...
@@ -66,15 +62,18 @@ const lcContigSeq = (S: string[], T: string[]): string[] => {
             if (S[i] === T[j]) {
                 if (i === 0 || j === 0) {
                     L[i][j] = 1;
-                }
-                else {
+                } else {
                     L[i][j] = L[i - 1][j - 1] + 1;
-                    if (L[i][j] > longestFound) { // Found a longer sequence
-                        longestFound = L[i][j];
-                        // todoret := {S[i − z + 1..i]}
-                    } else if (L[i][j] === longestFound) {
-                        // todo ret := ret ∪ {S[i − z + 1..i]}
-                    }
+                }
+
+                // Longest so far?
+                if (L[i][j] > longestFound) {
+                    longestFound = L[i][j];
+                    // Equivalent to ret := {S[i − z + 1..i]}
+                    ret = [S.slice(i - longestFound + 1, i + 1)];
+                } else if (L[i][j] === longestFound) {
+                    // Equivalent to ret := ret ∪ {S[i − z + 1..i]}
+                    ret.push(S.slice(i - longestFound + 1, i + 1))
                 }
             }
             else { // No match
@@ -83,7 +82,7 @@ const lcContigSeq = (S: string[], T: string[]): string[] => {
         }
     }
 
-    console.log(pretty(S, T, L));
+    // console.log(pretty(S, T, L));
 
     return ret;
 }
@@ -93,18 +92,31 @@ const lcContigSeq = (S: string[], T: string[]): string[] => {
  */
 const pretty = (S: string[], T: string[], lm: LongestMatrix) =>
     // Col headings in the 1st row
-    '\t'.repeat(2) + T.map(x => x.padEnd(10)).join('') + '\n'
-    + lm.map((r, i) =>
-        S[i].padEnd(10, ' ')
-        + r.map(x => x.toString().padEnd(10, ' ')).join('')
+    '\n\t'.repeat(2) + T.map(x => x.padEnd(10)).join('') + '\n'
+    + lm.map((r, i) => S[i].padEnd(10, ' ') + r.map(x => x.toString().padEnd(10, ' ')).join('')
     ).join('\n')
 
 
 /**
  * ASSERTIONS
  */
-console.log(JSON.stringify(
-    lcContigSeq(
-        ['/home', '/register', '/login', '/user', '/one', '/two'],
-        ['/home', '/red', '/login', '/user', '/one', '/pink'])
-) === `['/login', '/user', '/one']`);
+// No matches
+console.log(JSON.stringify(longestCommonContiguousSeq(
+    ['/hello', 'world'],
+    ['/home', '/red', '/user', '/one'])
+) === `[]`);
+
+console.log(JSON.stringify(longestCommonContiguousSeq(
+    ['/one'],
+    ['/home', '/red', '/user', '/one'])
+) === `[["/one"]]`);
+
+console.log(JSON.stringify(longestCommonContiguousSeq(
+    ['/home', '/register', '/login', '/user', '/one', '/two'],
+    ['/home', '/red', '/login', '/user', '/one', '/pink'])
+) === `[["/login","/user","/one"]]`);
+
+console.log(JSON.stringify(longestCommonContiguousSeq(
+    ['/home', '/register', '/login', '/user', '/one', '/two'],
+    ['/home', '/red', '/user', '/one'])
+) === `[["/user","/one"]]`);

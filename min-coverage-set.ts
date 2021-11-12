c6eec5e0f6583d151e737a96b9c4a050658a4a61
xtango <xtango@users.noreply.github.com>
2021-11-12 13:07:21 -0500

    Update min-coverage-set.ts

--- min-coverage-set.ts
+++ min-coverage-set.ts
@@ -6,13 +6,13 @@
  * Write a function fewest(prefs) that returns back the colors as an array.
  * 
  * For example:
- * Given:  buttonColorPreferences = {
- *        Maurice: [ Red, White, Blue],
- *        Tina: [ Green],
- *        Anita: [ White, Yellow] // which means Anita is good with White or Yellow
- *  }
+ * Input:  buttonColorPreferences = {
+ *           Maurice: [ Red, White, Blue],
+ *           Tina: [ Green],
+ *           Anita: [ White, Yellow] // which means Anita is good with White or Yellow
+ *         }
  *
- *  fewest(buttonColorPreferences) should return ['White', 'Green']
+ * Output: fewest(buttonColorPreferences) should return ['White', 'Green']
  */
 
 /**
@@ -61,9 +61,8 @@ const fewest = (prefs: string[]) => {
 console.log(combinations('rgb'))
 
 /**
- * 
- *  Anita: [ White, Yellow] // which means Anita is good with White or Yellow* Maurice: [ Red, White, Blue],
- *  Tina: [ Green],
+ * Anita: [ White, Yellow] // which means Anita is good with White or Yellow* Maurice: [ Red, White, Blue],
+ * Tina: [ Green],
  * Maurice: [ Red, White, Blue],
  */
 console.log(fewest(['wy', 'g', 'rwb']) === 'gw')

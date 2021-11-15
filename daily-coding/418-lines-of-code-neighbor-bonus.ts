29af22fc43ab4df981e5fe6c10c0dd0a1440148c
xtango <xtango@users.noreply.github.com>
2021-11-14 19:07:36 -0500

    Update 418-lines-of-code-neighbor-bonus.ts

--- daily-coding/418-lines-of-code-neighbor-bonus.ts
+++ daily-coding/418-lines-of-code-neighbor-bonus.ts
@@ -16,19 +16,76 @@
  * should return [1, 2, 3, 4, 2, 1].
  */
 
-const bonuses = (linesOfCode: number[]): number[] => {
-    console.log('loc', linesOfCode);
-    const payList = new Array(linesOfCode.length).fill(1);
+interface Band { direction: number, startIdx: number, endIdx: number};
+/**
+ * Breaks up the series into rising and falling segments.
+ * Returns the start and end indices of each band
+ */
+const getBands = (linesOfCode: number[]): Band []  => {
+    const bands: Band[] = []// list of start, end
+    let direction = linesOfCode[1] > linesOfCode[0]
+        ? 1 // rising
+        : linesOfCode[1] < linesOfCode[0] ? -1 //  falling
+            : 0;
+    let startIdx = 0;
     for (let i = 1; i < linesOfCode.length; i++) {
-        // Greater than previous, bump up the bonus
-        if (linesOfCode[i] > linesOfCode[i - 1]) {
-            payList[i] = payList[i - 1] + 1;
-        } else if (linesOfCode[i] < linesOfCode[i - 1]
-            && payList[i] > payList[i - 1]) {
-            payList[i - 1] = payList[i] + 1;
+        const [prev, curr] = [linesOfCode[i-1], linesOfCode[i]];
+        if (curr > prev && direction === -1) {
+            // Turning point from falling to rising
+            bands.push({direction, startIdx, endIdx: i-1});
+            startIdx = i;
+            direction = 1;
+        } else if (curr < linesOfCode[i - 1] && direction === 1) {
+            // Turning point from rising to falling
+            bands.push({direction, startIdx, endIdx: i-1});
+            startIdx = i;
+            direction = -1;
         }
     }
+    // Last band
+    bands.push({direction, startIdx, endIdx: linesOfCode.length -1})
+    return bands;
+}
+
+/**
+ * Approach
+ *              |
+ *         |    |
+ *     |   |    |    |
+ *  |  |   |    |    |    |
+ * 10  40 200 1000  60   30
+ * 
+ * |--rising----|-falling-|
+ * For the rising band we start incrementing by 1 starting from $1.
+ * For the falling band we start decrement by 1 starting from $x where x is the lengtgh of the falling band.
+ */
+const bonuses = (linesOfCode: number[]): number[] => {
+    console.log('loc', linesOfCode);
+    const payList: number[] = [];
+    const bands = getBands(linesOfCode);
+    bands.forEach(band => {
+        const len = band.endIdx - band.startIdx + 1;
+        if (band.direction === 1) {
+            for (let i = 1; i <= len; i++) {
+                payList.push(i);
+            }
+        } else if (band.direction === -1) { // Falling
+            for (let i = len; i > 0; i--) {
+                payList.push(i);
+            }
+        } else if (band.direction === 0) { // Same as neighbor
+            const samePay = payList[payList.length -1];
+            for (let i = 1; i <= len; i++) {
+                payList.push(samePay);
+            }
+        }
+    })
+        
     return payList;
 }
 
-console.log(bonuses([10, 40, 200, 1000, 60, 30]))
+/**
+ * Assertions
+ */
+console.log(getBands([60, 10, 40, 200, 1000, 60, 30]).length === 3);
+console.log(bonuses([10, 40, 200, 1000, 60, 30]).join(',') === '1,2,3,4,2,1')

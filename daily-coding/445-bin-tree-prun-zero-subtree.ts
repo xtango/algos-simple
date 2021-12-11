e0fca691be67cc88c8232d72a4007746c118ef54
xtango <xtango@users.noreply.github.com>
2021-12-11 09:57:36 -0500

    Update 445-bin-tree-prun-zero-subtree.ts

--- daily-coding/445-bin-tree-prun-zero-subtree.ts
+++ daily-coding/445-bin-tree-prun-zero-subtree.ts
@@ -1,48 +1,100 @@
 /** 
-Problem #445 [Medium]
-
-This question was asked by BufferBox.
-
-Given a binary tree where all nodes are either 0 or 1, prune the tree so that subtrees containing all 0s are removed.
-
-For example, given the following tree:
-
-  0
- / \
-1   0
-   / \
-  1   0
- / \
-0   0
-should be pruned to:
-
-  0
- / \
-1   0
-   /
-  1
-We do not remove the tree at the root or its left child because it still has a 1 as a descendant.
-*/
-
+ *          Problem #445 [Medium] - BINARY TREE PRUNE ALL-ZERO SUBTREES
+ * 
+ * This question was asked by BufferBox.
+ * 
+ * Given a binary tree where all nodes are either 0 or 1, prune the tree
+ * so that subtrees containing all 0s are removed.
+ * For example, given the following tree:
+ *   0
+ *  / \
+ * 1   0
+ *    / \
+ *   1   0
+ *  / \
+ * 0   0
+ * should be pruned to:
+ *   0
+ *  / \
+ * 1   0
+ *    /
+ *   1
+ * We do not remove the tree at the root or its left child because it still
+ * has a 1 as a descendant.
+ * 
+ */
 interface BTNode { id: string, val: number, left?: BTNode, right?: BTNode }
 
 const pruneSubtreeZero = (bt: BTNode) => {
-    // Depth first traversal
-
-    const dft = (node: BTNode) => {
-        console.log('visit node', node.id, node.val);
-        if (node.left) {
-            dft(node.left);
+    const subTree = (node: BTNode) => {
+        if (node === undefined) {
+            return undefined;
         }
-        if (node.right) {
-            dft(node.right);
-        }
+        console.log(`visit node ${node.id}:${node.val}`);
+        node.left = subTree(node.left);
+        node.right = subTree(node.right);
         if (node.left === undefined && node.right === undefined && node.val === 0) {
-            console.log('remove', node.id);
+            console.log('\tremove');
+            return undefined;
+        }
+        return node;
+    }
+
+    subTree(bt);
+    return bt;
+}
+
+/**
+ * Returns IDs using an in-order (left, root (append to out), right) traversal.
+ * This function is used for the test cases only.
+ * 
+ * @example inOrder() returns ['a', 'b', 'c', 'd', 'e']
+ *     d:4       
+ *     /  \
+ *   b:2   e:5
+ *  /   \
+ * a:1   c:3
+ */
+const inOrder = (binTree: BTNode): string[] => {
+    const outIds: string[] = [];
+
+    const helper = (node: BTNode) => {
+        if (node === undefined) {
+            return;
+        }
+        if (node.left) {
+            helper(node.left);
+        }
+        outIds.push(node.id);
+        if (node.right) {
+            helper(node.right);
         }
     }
-    dft(bt);
+    helper(binTree);
+    return outIds;
 }
+
+
+/**
+ * ASSERTIONS
+ */
+/** 
+ *     d:4
+ *    /   \
+ *   b:2   e:5
+ *  /   \
+ * a:1   c:3
+ */
+const BT1 = {
+    id: 'd',
+    val: 4,
+    left: {
+        id: 'b', val: 2,
+        left: { id: 'a', val: 1 },
+        right: { id: 'c', val: 3 }
+    },
+    right: { id: 'e', val: 5 }
+};
 
 /**
  *    a:0
@@ -53,7 +105,7 @@ const pruneSubtreeZero = (bt: BTNode) => {
  *  /  \
  * e:0  f: 0
  */
-const BT1 = {
+const BT2 = {
     id: 'a',
     val: 0,
     left: { id: 'b', val: 1 },
@@ -70,4 +122,8 @@ const BT1 = {
     }
 }
 
-pruneSubtreeZero(BT1);
+// Test inOrder
+console.log(inOrder(BT1).join('') ==='abcde');
+
+// Test pruneSubtreeZero
+console.log(inOrder(pruneSubtreeZero(BT2)).join('') === 'badc');

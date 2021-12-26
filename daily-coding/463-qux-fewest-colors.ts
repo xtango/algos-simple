51620b602a6f85e7a84e91290e276e64c477b390
xtango <xtango@users.noreply.github.com>
2021-12-26 15:09:48 -0500

    Update 463-qux-fewest-colors.ts

--- daily-coding/463-qux-fewest-colors.ts
+++ daily-coding/463-qux-fewest-colors.ts
@@ -1,5 +1,5 @@
 /**
- *                          Problem #463 [Easy] - QUXES MERGE COLORS 
+ *                    Problem #463 [Easy] - QUXES MERGE CREATURE COLORS
  * 
  * This problem was asked by Facebook.
  * 
@@ -12,49 +12,62 @@
  * 
  * For example, given the input ['R', 'G', 'B', 'G', 'B'], it is possible to end up with
  * a single Qux through the following steps:
+ * 
+ *         Arrangement       |   Change
+ * ----------------------------------------
+ * ['R', 'G', 'B', 'G', 'B'] | (R, G) -> B
+ * ['B', 'B', 'G', 'B']      | (B, G) -> R
+ * ['B', 'R', 'B']           | (R, B) -> G
+ * ['B', 'G']                | (B, G) -> R
+ * ['R']                     |
+ */
 
-        Arrangement       |   Change
-----------------------------------------
-['R', 'G', 'B', 'G', 'B'] | (R, G) -> B
-['B', 'B', 'G', 'B']      | (B, G) -> R
-['B', 'R', 'B']           | (R, B) -> G
-['B', 'G']                | (B, G) -> R
-['R']                     |
-*
-*/
+/**
+ * Converts the color to a creature-number (R: 0, G: 1, B: 2) to make merging simpler
+ */
 const toNum = (color: string): number => color === 'R' ? 0 : color === 'G' ? 1 : 2;
 
+/**
+ * Returns the merged creature number.
+ */
 const mergePair = (creature1: number, creature2: number): number => 3 - creature1 - creature2;
 
+/**
+ * Returns the shortest length of remaining creatures after merging.
+ */
 const quxMerge = (input: string[]): number => {
-    let shortest = input.length;
 
-    const mergeHelper = (creatures: number[], depth: number = 0): number => {
+    const mergeCreatures = (creatures: number[], depth: number = 0): number => {
+
         if (depth > 5) {
             return -1; // Short circuit
         }
-        console.log(`[${depth}] creatures: ${creatures.join(',')}`);
-        for (let i = 0; i < creatures.length; i++) {
+        let minRemaining = creatures.length;
+
+        // console.log(`[depth: ${depth}] mergeCeatures: ${creatures.join(' ')}`);
+        for (let i = 0;
+            i < creatures.length -1; // Stop at 2nd last 
+            i++) {
+            // console.log(`\t\t[i: ${i}/${creatures.length - 1}] ${creatures.join(' ')}`);
             if (creatures[i] !== creatures[i + 1]) {
                 const left = creatures.slice(0, i); // '' -> 
-                const transformed = mergePair(creatures[i], creatures[i + 1]);
-                const right = creatures.slice(i + 2);
-                const len = mergeHelper([...left, transformed, ...right], depth + 1);
-                if (len < shortest) {
-                    shortest = len;
-                }
+                const newCreature = mergePair(creatures[i], creatures[i + 1]);
+                const right = i < creatures.length - 2 ? creatures.slice(i + 2) : [];
+                 // console.log(`\t\t\t(left: ${left.join(' ')}) + (new: ${newCreature}) + (right: ${right.join(' ')})`);
+                const newCreatures = [...left, newCreature, ...right];
+                minRemaining = Math.min(minRemaining, mergeCreatures(newCreatures, depth + 1));
             }
         }
-        return shortest;
+        return minRemaining;
     }
 
-    return mergeHelper(
-        input.map(x => toNum(x)) // Version of input converted to numbers
+    return mergeCreatures(
+        input.map(x => toNum(x)) // input converted to creature numbers
     );
 }
 
 /**
  * ASSERTIONS
  */
-
-console.log(quxMerge(['R', 'G', 'B', 'G', 'B']))
+console.log(quxMerge(['R', 'G', 'B', 'G', 'B']) === 1);
+console.log(quxMerge(['R', 'R']) === 2);

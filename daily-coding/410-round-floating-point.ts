75e4324305347afb8f7b08cd2df69151ba4539cb
unknown <xcursetjee@gmail.com>
2021-12-30 19:04:52 -0500

    410 round floating points

--- daily-coding/410-round-floating-point.ts
+++ daily-coding/410-round-floating-point.ts
@@ -14,6 +14,22 @@ For example, suppose your input is [1.3, 2.3, 4.4]. In this case you cannot do b
 which has an absolute difference of |1.3 - 1| + |2.3 - 2| + |4.4 - 5| = 1.2
 */
 
+/**
+ * Uses ceil and floor
+ */
+ const roundUpOrDown = (arr: number[]): number[] => {
+    const helper = (accum: number[], i:number): number => {
+        console.log(`[i: ${i}] ${arr[i]}`);
+        if (i >= arr.length) {
+            return 0;
+        }
+        const accumCeil = Math.ceil(arr[i]) + helper(accum, i + 1);
+        // const accumFloor = [...accum].push(Math.floor(arr[i]));
+        return accumCeil;
+    }
+    return []
+}
+
 const absoluteDiff = (arr1: number[], arr2: number[]): number => {
     return arr1.reduce((accum, elem1, idx) => {
         accum += Math.abs(elem1 - arr2[idx]);
@@ -21,7 +37,4 @@ const absoluteDiff = (arr1: number[], arr2: number[]): number => {
     }, 0);
 }
 
-/**
- * 
- */
 console.log(absoluteDiff([1.3, 2.3, 4.4], [1, 2, 5]))

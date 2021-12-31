a156142f2df2515db93c897cad1ad2cff7aa6b69
xtango <xtango@users.noreply.github.com>
2021-12-31 12:52:22 -0500

    Update 410-round-floating-point.ts

--- daily-coding/410-round-floating-point.ts
+++ daily-coding/410-round-floating-point.ts
@@ -1,28 +1,25 @@
 /**
- *                          #410 [Hard] - ROUND FLOATING-POINT
-
-This problem was asked by Airbnb.
-
-You are given an array X of floating-point numbers x1, x2, ... xn. These can be rounded up or down to create a corresponding array Y of integers y1, y2, ... yn.
-
-Write an algorithm that finds an appropriate Y array with the following properties:
-
-The rounded sums of both arrays should be equal.
-The absolute pairwise difference between elements is minimized. In other words, |x1- y1| + |x2- y2| + ... + |xn- yn| should be as small as possible.
-
-For example, suppose your input is [1.3, 2.3, 4.4]. In this case you cannot do better than [1, 2, 5],
-which has an absolute difference of |1.3 - 1| + |2.3 - 2| + |4.4 - 5| = 1.2
-*/
+ *                          #410 [Hard] - ROUND FLOATING-POINT ARRAY
+ * 
+ * This problem was asked by Airbnb.
+ * 
+ * You are given an array X of floating-point numbers x1, x2, ... xn.
+ * These can be rounded up or down to create a corresponding array Y of integers y1, y2, ... yn.
+ * Write an algorithm that finds an appropriate Y array with the following properties:
+ *      The rounded sums of both arrays should be equal.
+ *      The absolute pairwise difference between elements is minimized.
+ *      In other words, |x1- y1| + |x2- y2| + ... + |xn- yn| should be as small as possible.
+ * 
+ * For example, suppose your input is [1.3, 2.3, 4.4]. In this case you cannot do better than [1, 2, 5],
+ * which has an absolute difference of |1.3 - 1| + |2.3 - 2| + |4.4 - 5| = 1.2
+ */
 
 interface SearchResult { diff: number, path: number[] }
-const NOT_FOUND: SearchResult = {
-    diff: Number.POSITIVE_INFINITY,
-    path: []
-};
+
+const NOT_FOUND: SearchResult = { diff: Number.POSITIVE_INFINITY, path: [] };
 
 /**
  * Uses ceil and floor
- *                          
  *                         / (4.4->5)
  *                (2.3->3)
  *               /         \ (4.4->4)
@@ -32,46 +29,45 @@ const NOT_FOUND: SearchResult = {
  *                        \ (4.4->4)
  * 
  *      (1.3->1) ....            
- *  */
-const roundUpOrDown = (inputFloatArr: number[]): number => {
-    const sumInputFloatArrRounded = inputFloatArr.reduce((accum, x) => {
-        accum += Math.round(x);
-        return accum;
-    }, 0)
+ */
+const roundUpDownArray = (inputFloatArr: number[]): number[] => {
+    const inputFloatArrRoundedSum = roundedSum(inputFloatArr);
 
+    /**
+     * Function to help recursion
+     * 
+     * @param cumulRoundedSum The cumulative sum of the rounded values. Needed to check 
+     *                         for "The rounded sums of both arrays should be equal."
+     * 
+     * @param cumulDiff The cumulative sum of |xn- yn|. Needed to check for 
+     *                  "the absolute pairwise difference between elements is minimized."
+     */
     const helper = (
-        floatArrSubset: number[],
-        roundedArrSubset: number[],
+        i: number,
+        roundedArrSubset: number[] = [],
         cumulRoundedSum: number = 0,
         cumulDiff: number = 0
     ): SearchResult => {
-        if (floatArrSubset === undefined) {
-            // went past last
-            return (cumulRoundedSum === sumInputFloatArrRounded)
-                ? NOT_FOUND
-                : {
+        console.log(`[i: ${i}]`);
+        // Base case: when hit last, check "The rounded sums of both arrays should be equal."
+        if (i > inputFloatArr.length - 1) {
+            console.log('\tHit last. Rounded sums equal?', cumulRoundedSum, inputFloatArrRoundedSum )
+            return (cumulRoundedSum === inputFloatArrRoundedSum)
+                ? {
                     diff: cumulDiff,
                     path: roundedArrSubset
-                };
+                }
+                : NOT_FOUND;
         }
-        const [curr, currCeil, currFloor] = [
-            floatArrSubset[0],
-            Math.ceil(floatArrSubset[0]),
-            Math.floor(floatArrSubset[0])];
 
-        console.log(`[curr: ${curr}]`);
+        const curr = inputFloatArr[i];
+        const [currCeil, currFloor] = [Math.ceil(curr), Math.floor(curr)];
+
+        console.log(`\t floor, curr, ceil: ${currFloor} ${curr} ${currCeil}`);
 
         // recurse
-        const restUp = helper(
-            floatArrSubset.slice(1),
-            [currCeil, ...roundedArrSubset],
-            cumulDiff + Math.abs(curr - currCeil));
-
-        const restDown = helper(
-            floatArrSubset.slice(1),
-            [currFloor, ...roundedArrSubset],
-            cumulDiff + Math.abs(curr - currFloor));
-
+        const restUp = helper(i + 1, [currCeil, ...roundedArrSubset], cumulDiff + Math.abs(curr - currCeil));
+        const restDown = helper(i + 1, [currFloor, ...roundedArrSubset], cumulDiff + Math.abs(curr - currFloor));
         const best = restUp.diff < restDown.diff ? restUp : restDown;
 
         return {
@@ -79,10 +75,12 @@ const roundUpOrDown = (inputFloatArr: number[]): number => {
             path: best.path
         }
     }
+
+    return helper(0).path;
 }
 
-return helper(0, true);
-}
+
+const roundedSum = (arr: number[]) => arr.reduce((accum, x) => accum + Math.round(x), 0);
 
 const absoluteDiff = (arr1: number[], arr2: number[]): number => {
     return arr1.reduce((accum, elem1, idx) => {
@@ -91,5 +89,9 @@ const absoluteDiff = (arr1: number[], arr2: number[]): number => {
     }, 0);
 }
 
+/**
+ * ASSERTIONS
+ */
+console.log(roundedSum([1.3, 2.3, 4.4]) === 7);
 // console.log(absoluteDiff([1.3, 2.3, 4.4], [1, 2, 5]))
-console.log(roundUpOrDown([1.3, 2.3, 4.4]))
+ console.log(roundUpDownArray([1.3, 2.3, 4.4]))

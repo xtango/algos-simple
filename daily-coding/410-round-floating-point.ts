9047eef909034a3e4896d847d04235aa9b70ea64
xtango <xtango@users.noreply.github.com>
2021-12-31 17:56:36 -0500

    Update 410-round-floating-point.ts

--- daily-coding/410-round-floating-point.ts
+++ daily-coding/410-round-floating-point.ts
@@ -19,25 +19,27 @@ interface SearchResult { diff: number, path: number[] }
 const NOT_FOUND: SearchResult = { diff: Number.POSITIVE_INFINITY, path: [] };
 
 /**
- * Uses ceil and floor
+ * This implementation recursively traverses all paths. The top is the ceil
+ * and the bottom is the floor.
+ * 
  *                         / (4.4->5)
  *                (2.3->3)
  *               /         \ (4.4->4)
  *      (1.3->2)             
- *               \        / (4.4->5)
- *                (2.3->2)
- *                        \ (4.4->4)
- * 
- *      (1.3->1) ....            
+ *    /          \         / (4.4->5)
+ *   /            (2.3->2)
+ *   \                     \ (4.4->4)
+ *    \ 
+ *      (1.3->1) ...    
  */
 const roundUpDownArray = (inputFloatArr: number[]): number[] => {
     const inputFloatArrRoundedSum = roundedSum(inputFloatArr);
 
     /**
-     * Function to help recursion
+     * Helper function for recursive calls.
      * 
      * @param cumulRoundedSum The cumulative sum of the rounded values. Needed to check 
-     *                         for "The rounded sums of both arrays should be equal."
+     *                        for "The rounded sums of both arrays should be equal."
      * 
      * @param cumulDiff The cumulative sum of |xn- yn|. Needed to check for 
      *                  "the absolute pairwise difference between elements is minimized."
@@ -48,11 +50,16 @@ const roundUpDownArray = (inputFloatArr: number[]): number[] => {
         cumulRoundedSum: number = 0,
         cumulDiff: number = 0
     ): SearchResult => {
-        console.log(`[i: ${i}]`);
-        // Base case: when hit last, check "The rounded sums of both arrays should be equal."
-        if (i > inputFloatArr.length - 1) {
-            console.log('\tHit last. Rounded sums equal?', cumulRoundedSum, inputFloatArrRoundedSum )
-            return (cumulRoundedSum === inputFloatArrRoundedSum)
+        console.log(`[i: ${i}] path: ${roundedArrSubset.join(',')}`);
+
+        // Base case: when past last array elem,
+        //            check "The rounded sums of both arrays should be equal."
+        if (i >= inputFloatArr.length) {
+            const roundedSumsEqual = cumulRoundedSum === inputFloatArrRoundedSum;
+            console.log(`\tEnd. Rounded sums ${cumulRoundedSum} =? ${inputFloatArrRoundedSum} ${roundedSumsEqual}`);
+            console.log(`\tDiff: ${cumulDiff}`);
+             
+            return roundedSumsEqual
                 ? {
                     diff: cumulDiff,
                     path: roundedArrSubset
@@ -62,36 +69,39 @@ const roundUpDownArray = (inputFloatArr: number[]): number[] => {
 
         const curr = inputFloatArr[i];
         const [currCeil, currFloor] = [Math.ceil(curr), Math.floor(curr)];
-
         console.log(`\t floor, curr, ceil: ${currFloor} ${curr} ${currCeil}`);
 
         // recurse
-        const restUp = helper(i + 1, [currCeil, ...roundedArrSubset], cumulDiff + Math.abs(curr - currCeil));
-        const restDown = helper(i + 1, [currFloor, ...roundedArrSubset], cumulDiff + Math.abs(curr - currFloor));
-        const best = restUp.diff < restDown.diff ? restUp : restDown;
+        const ceilResult = helper(
+            i + 1,
+            [...roundedArrSubset, currCeil],
+            cumulRoundedSum + currCeil,
+            cumulDiff + Math.abs(curr - currCeil));
+        const floorResult = helper(
+            i + 1,
+            [...roundedArrSubset, currFloor],
+            cumulRoundedSum + currFloor,
+            cumulDiff + Math.abs(curr - currFloor));
+        const minDiff = ceilResult.diff < floorResult.diff ? ceilResult : floorResult;
 
-        return {
-            diff: best.diff,
-            path: best.path
-        }
+        return { diff: minDiff.diff, path: minDiff.path }
     }
 
     return helper(0).path;
 }
 
-
+/**
+ * Note that the javascript round function differs from many other languages
+ */
 const roundedSum = (arr: number[]) => arr.reduce((accum, x) => accum + Math.round(x), 0);
-
-const absoluteDiff = (arr1: number[], arr2: number[]): number => {
-    return arr1.reduce((accum, elem1, idx) => {
-        accum += Math.abs(elem1 - arr2[idx]);
-        return accum;
-    }, 0);
-}
 
 /**
  * ASSERTIONS
  */
 console.log(roundedSum([1.3, 2.3, 4.4]) === 7);
-// console.log(absoluteDiff([1.3, 2.3, 4.4], [1, 2, 5]))
- console.log(roundUpDownArray([1.3, 2.3, 4.4]))
+
+// Note: The answer expected in the problem statement is [1, 2, 5], 
+//       which sums up to 8 instead of 7 and has an absolute difference of 
+//       |1.3 - 1| + |2.3 - 2| + |4.4 - 5| = 1.2
+//       The diff we get from the implemented above is Diff: 1.0000000000000002
+console.log(roundUpDownArray([1.3, 2.3, 4.4]).join(',') === '1,2,4')

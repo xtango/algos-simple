0ddf611ba3dbc9c0f6779aa107e4f9a73d425b74
xtango <xtango@users.noreply.github.com>
2022-01-05 20:30:24 -0500

    Update 473-directed-graph-reversal.ts

--- daily-coding/473-directed-graph-reversal.ts
+++ daily-coding/473-directed-graph-reversal.ts
@@ -1,8 +1,6 @@
 /**
                     Problem #473 [Medium] - REVERSE DIRECTED GRAPH
-
 This problem was asked by Yahoo.
-
 Write an algorithm that computes the reversal of a directed graph. 
 For example, if a graph consists of A -> B -> C, it should become A <- B <- C.
 */
@@ -26,17 +24,31 @@ const reverseDG = (g: DG): DG => {
 
 const pretty = (g: DG) => JSON.stringify(g);
 
+const googleChartURL = (g: DG) => {
+    const params = Object.entries(g)
+        .map(([node, adjNodes]) => `${adjNodes.length ? node + '->' + adjNodes.join(',') : ''}`)
+        .filter(x=> x !== '')
+        .join(';');
+    return `https://chart.googleapis.com/chart?cht=gv&chl=digraph{${params}}&`
+}
+
 /**
  * ASSERTIONS
  */
-console.log(pretty(reverseDG({
+const DG1 = {
     'A': ['B'],
     'B': ['C'],
     'C': []
-})) === `{"A":[],"B":["A"],"C":["B"]}`);
+};
+console.log(pretty(reverseDG(DG1)) === `{"A":[],"B":["A"],"C":["B"]}`);
+console.log(googleChartURL(DG1));
+console.log(googleChartURL(reverseDG(DG1)));
 
-console.log(pretty(reverseDG({
+const DG2 = {
     'A': ['B', 'C'],
     'B': ['C'],
     'C': []
-})) === `{"A":[],"B":["A"],"C":["A","B"]}`);
+};
+console.log(pretty(reverseDG(DG2)) === `{"A":[],"B":["A"],"C":["A","B"]}`);
+console.log(googleChartURL(DG2));
+console.log(googleChartURL(reverseDG(DG2)));

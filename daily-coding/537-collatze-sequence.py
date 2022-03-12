bcbca98dfdd9dfc5633e3b7d121877e98d40aceb
Xerxes Cursetjee <xtango@users.noreply.github.com>
2022-03-12 16:05:38 +0000

    changed indent size to 4

--- daily-coding/537-collatze-sequence.py
+++ daily-coding/537-collatze-sequence.py
@@ -10,21 +10,27 @@ It is conjectured that every such sequence eventually reaches the number 1. Test
 
 Bonus: What input n <= 1000000 gives the longest sequence?
 '''
+
+
 def next_num(n: int) -> int:
-  return n / 2 if n % 2 == 0 else 3 * n + 1
+    return n / 2 if n % 2 == 0 else 3 * n + 1
+
 
 '''
 Tests the conjecture that the sequence reaches 1.
 '''
-def reaches_one(start_num: int, max_iteration = 10000) -> bool:
-  i, new_num = 0, start_num
-  while(i < max_iteration and new_num != 1):
-    new_num = next_num(new_num)
-    print(i, new_num)
-    i = i + 1
-  return (new_num == 1)
-  
+
+
+def reaches_one(start_num: int, max_iteration=10000) -> bool:
+    i, new_num = 0, start_num
+    while (i < max_iteration and new_num != 1):
+        new_num = next_num(new_num)
+        print(i, new_num)
+        i = i + 1
+    return (new_num == 1)
+
+
 # ASSERTIONS
-assert(next_num(100) == 50)
-assert(next_num(101) == 304)
-assert(reaches_one(start_num = 100))
+assert (next_num(100) == 50)
+assert (next_num(101) == 304)
+assert (reaches_one(start_num=100))

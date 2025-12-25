72eb19eb5c804719b174005734480b9eb3814a94
xtango <xtango@users.noreply.github.com>
2025-12-24 22:59:01 -0500

    Refactor open lock algorithm with bi-directional BFS
    Refactor open lock algorithm to improve clarity and performance. Added bi-directional BFS implementation for efficiency.

--- ulysses/open-lock.ts
+++ ulysses/open-lock.ts
@@ -1,65 +1,13 @@
 /**
- * Turns each of the 4 digits 1 place up and down, returning 8 new combinations
- * example: nextStates('0000') -> ["1000", "9000", "0100", "0900", "0010", "0090", "0001", "0009"] 
+ *                        OPEN THE LOCK
+ *
+ * Problem statement: https://leetcode.com/problems/open-the-lock/description/
  */
-const UP = { '0': '1', '1': '2', '2': '3', '3': '4', '4': '5', '5': '6', '6': '7', '7': '8', '8': '9', '9': '0' }
-const DOWN = { '0': '9', '1': 0, '2': '1', '3': '2', '4': '3', '5': '4', '6': '5', '7': '6', '8': '7', '9': '8' }
-
-const nextCombinations = (curr: string): string[] => {
-    const states = []
-    for (let i = 0; i < 4; i++) {
-        const digit = curr[i];
-        const left = curr.substring(0, i);
-        const right = curr.substring(i + 1)
-        const up = left + UP[digit] + right
-        const down = left + DOWN[digit] + right
-        states.push(up, down)
-    }
-    return states;
-}
 
 /**
- * BFS Search. This does NOT use a bi-directional BFS, so it is slow for certain cases and can time out. 
- */
-const openLockSingleDirectional = (deadends: string[], target: string): number => {
-    const CIRCUIT_BREAKER = 300;
-
-    if (target === "0000") return 0
-
-    const seen = new Uint8Array(10000)
-    // Mark deadends in seen array
-    for (let d of deadends)
-        seen[parseInt(d)] = 1
-
-    // BFS
-    const q: [string, number][] = [['0000', 0]]; // tuple of (digits, numTurns)
-    let iter = 0;
-    while (q.length > 0 && iter < CIRCUIT_BREAKER) {
-        console.log('iter, lenq', iter, q.length)
-        const [digits, turns] = q.shift(); // our q goes from right to left
-        if (digits === target) {
-            return turns;
-        }
-        // const combs = nextCombinations(digits)
-        const combs = getNeighbors(digits);
-        console.log('combs for', digits, combs)
-        combs.forEach(c => {
-            const digitsAsInt = parseInt(c);
-            if (!seen[digitsAsInt]) {
-                seen[digitsAsInt] = 1;
-                q.push([c, turns + 1]);
-                iter++;
-            }
-        })
-    }
-
-    return -1; // not possible
-}
-
-
-/**
- * Bi-directional. Generally faster, but more complicated
- * We start with q1 initialized to '0000' and q2 initialized to the target.
+ * Bi-directional BFS. Faster for large N, but more complicated.
+ * We use 2 queues. We start with q1 initialized to '0000' and q2 initialized to the target.
+ * See video: https://www.youtube.com/watch?v=a_uRLWLI5ug
  */
 const openLockBiDirectional = (deadends: string[], target: string): number => {
     let deads = new Set(deadends);
@@ -140,6 +88,65 @@ const getNeighbors= (s: string): string[] => {
         neighbors.push(minusOne(s, i));
     }
     return neighbors;
+}
+
+
+/**
+ * Turns each of the 4 digits 1 place up and down, returning 8 new combinations
+ * example: nextStates('0000') -> ["1000", "9000", "0100", "0900", "0010", "0090", "0001", "0009"] 
+ */
+const UP = { '0': '1', '1': '2', '2': '3', '3': '4', '4': '5', '5': '6', '6': '7', '7': '8', '8': '9', '9': '0' }
+const DOWN = { '0': '9', '1': 0, '2': '1', '3': '2', '4': '3', '5': '4', '6': '5', '7': '6', '8': '7', '9': '8' }
+
+const nextCombinations = (curr: string): string[] => {
+    const states = []
+    for (let i = 0; i < 4; i++) {
+        const digit = curr[i];
+        const left = curr.substring(0, i);
+        const right = curr.substring(i + 1)
+        const up = left + UP[digit] + right
+        const down = left + DOWN[digit] + right
+        states.push(up, down)
+    }
+    return states;
+}
+
+/**
+ * BFS Search. This does NOT use a bi-directional BFS, so it is slow for certain cases and can time out. 
+ */
+const openLockSingleDirectional = (deadends: string[], target: string): number => {
+    const CIRCUIT_BREAKER = 300;
+
+    if (target === "0000") return 0
+
+    const seen = new Uint8Array(10000)
+    // Mark deadends in seen array
+    for (let d of deadends)
+        seen[parseInt(d)] = 1
+
+    // BFS
+    const q: [string, number][] = [['0000', 0]]; // tuple of (digits, numTurns)
+    let iter = 0;
+    while (q.length > 0 && iter < CIRCUIT_BREAKER) {
+        console.log('iter, lenq', iter, q.length)
+        const [digits, turns] = q.shift(); // our q goes from right to left
+        if (digits === target) {
+            return turns;
+        }
+        // const combs = nextCombinations(digits)
+        const combs = getNeighbors(digits);
+        console.log('combs for', digits, combs)
+        combs.forEach(c => {
+            const digitsAsInt = parseInt(c);
+            if (!seen[digitsAsInt]) {
+                seen[digitsAsInt] = 1;
+                q.push([c, turns + 1]);
+                iter++;
+            }
+        })
+    }
+
+    return -1; // not possible
 }
 
 
